f4d50fe02edbe186ecc40f2d95c02c36b2bf6b9e
Oleg Godovykh <olegg.lieangel@gmail.com>
2015-12-18 22:23:22 -0500

    solve 4th day 1st task

---
+++ src/main/kotlin/day4/Day4.kt
@@ -0,0 +1,22 @@
+package day4
+
+import someday.SomeDay
+import java.math.BigInteger
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+
+/**
+ * Created by olegg on 12/18/15.
+ */
+class Day4: SomeDay(4) {
+    override fun first(): String {
+        return sequence(1) { it + 1 }.first { (data + it.toString()).md5().startsWith("00000") }.toString()
+    }
+}
+
+fun String.md5() = "%032x".format(BigInteger(1, MessageDigest.getInstance("MD5").digest(this.toByteArray(StandardCharsets.UTF_8))))
+
+fun main(args: Array<String>) {
+    val day = Day4()
+    println(day.first())
+}
