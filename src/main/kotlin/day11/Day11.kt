536ed507b793ada92d438bb0a4ecf7668999388d
Oleg Godovykh <olegg.lieangel@gmail.com>
2015-12-19 23:56:58 -0500

    solve 11th day 1st task

---
+++ src/main/kotlin/day11/Day11.kt
@@ -0,0 +1,34 @@
+package day11
+
+import someday.SomeDay
+import utils.series
+
+/**
+ * Created by olegg on 12/19/15.
+ */
+class Day11: SomeDay(11) {
+    fun passwordList(password: String) = sequence(password) {
+        it.toList().foldRight(Pair("", true)) { value, acc ->
+            Pair(
+                    (if (!acc.second) value else when (value) {
+                        'z' -> 'a'
+                        else -> value + 1
+                    }) + acc.first,
+                    acc.second && value == 'z'
+            )
+        }.first
+    }
+
+    override fun first(): String {
+        return passwordList(data).drop(1)
+                .filterNot { string -> "iol".any { string.contains(it) } }
+                .filter { string -> ('a'..'x').map { String(charArrayOf(it, it + 1, it + 2)) }.any { string.contains(it) } }
+                .filter { it.toList().series().filter { it.size > 1 }.flatMap { it }.joinToString(separator = "").length > 3 }
+                .first()
+    }
+}
+
+fun main(args: Array<String>) {
+    val day = Day11()
+    println(day.first())
+}
