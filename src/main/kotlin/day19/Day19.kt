f1d2972fb89c660c050f18b0fa985b2524e99b97
Oleg Godovykh <ogodovykh@eastbanctech.com>
2015-12-21 12:39:38 -0500

    solve 19th day 2nd task

--- src/main/kotlin/day19/Day19.kt
+++ src/main/kotlin/day19/Day19.kt
@@ -22,9 +22,24 @@ class Day19: SomeDay(19) {
     override fun first(): String {
         return transitions.map { it.first.toPattern() to it.second }.flatMap { applyTransitions(molecule, it) }.toSet().size.toString()
     }
+
+    override fun second(): String {
+        val reverse = transitions.map { it.second.toPattern() to it.first }
+        val molecules = hashMapOf(molecule to 0)
+        val queue = linkedListOf(molecule)
+        while (!molecules.containsKey("e") && queue.isNotEmpty()) {
+            val curr = queue.pop()
+            val size = molecules.getOrDefault(curr, Int.MAX_VALUE)
+            val next = reverse.flatMap { applyTransitions(curr, it) }.sortedBy { it.length }.take(2) // greedy approach
+            next.filterNot { molecules.containsKey(it) }.forEach { queue.addFirst(it) }
+            molecules += next.map { it to (molecules.getOrDefault(it, Int.MAX_VALUE)).coerceAtMost(size + 1) }.toMap()
+        }
+        return molecules.getOrDefault("e", Int.MAX_VALUE).toString()
+    }
 }
 
 fun main(args: Array<String>) {
     val day = Day19()
     println(day.first())
+    println(day.second())
 }
