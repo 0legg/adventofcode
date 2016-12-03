63bd0302d83ba02145a32ff9782909c839d0f190
Oleg Godovykh <olegg.lieangel@gmail.com>
2016-12-02 22:21:11 -0800

    use retrofit scalars library

--- src/main/kotlin/someday/Fetcher.kt
+++ src/main/kotlin/someday/Fetcher.kt
@@ -1,13 +1,11 @@
 package someday
 
-import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.Converter
 import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
-import java.lang.reflect.Type
 
 /**
  * Created by olegg on 12/18/15.
@@ -20,9 +18,7 @@ interface Fetcher {
     companion object {
         val fetcher  = Retrofit.Builder()
                 .baseUrl("http://adventofcode.com")
-                .addConverterFactory(object : Converter.Factory() {
-                    override fun responseBodyConverter(type: Type?, annotations: Array<out Annotation>?, retrofit: Retrofit?) = Converter<ResponseBody, String> { it.string() }
-                })
+                .addConverterFactory(ScalarsConverterFactory.create())
                 .build()
                 .create(Fetcher::class.java)
     }
