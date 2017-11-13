1e5d7f086e38176e490e03282143eb1faf07011a
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-11-12 22:48:15 -0800

    setup cookie via gradle.properties

--- src/main/kotlin/net/olegg/adventofcode/someday/Fetcher.kt
+++ src/main/kotlin/net/olegg/adventofcode/someday/Fetcher.kt
@@ -1,5 +1,6 @@
 package net.olegg.adventofcode.someday
 
+import net.olegg.adventofcode.BuildConfig
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
@@ -11,7 +12,7 @@ import retrofit2.http.Path
  * Created by olegg on 12/18/15.
  */
 interface Fetcher {
-    @Headers("Cookie: session=53616c7465645f5f310b83cb3f4bda451297cb2eea755f9c4c2c96d60464a7a7bdc7b60e9ca1eb6e313df542931107bb")
+    @Headers("Cookie: session=${BuildConfig.COOKIE}")
     @GET("{year}/day/{day}/input")
     fun fetchInput(@Path("year") year: Int, @Path("day") day: Int): Call<String>
 
