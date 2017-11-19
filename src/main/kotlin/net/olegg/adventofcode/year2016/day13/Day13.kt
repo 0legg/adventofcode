da2338b58793d4f615b50ac32a685d4b02390f41
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-11-18 22:10:52 -0800

    solve 13th day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2016/day13/Day13.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2016/day13/Day13.kt
@@ -3,6 +3,9 @@ package net.olegg.adventofcode.year2016.day13
 import net.olegg.adventofcode.someday.SomeDay
 import net.olegg.adventofcode.year2016.DayOf2016
 
+/**
+ * @see <a href="http://adventofcode.com/2016/day/13">Year 2016, Day 13</a>
+ */
 class Day13 : DayOf2016(13) {
     override fun first(data: String): String {
         val fav = data.toInt()
@@ -16,7 +19,7 @@ class Day13 : DayOf2016(13) {
 
         val target = 31 to 39
 
-        val known = mutableMapOf<Pair<Int, Int>, Int>()
+        val known = mutableMapOf((1 to 1) to 0)
         val queue = mutableListOf(Triple(1, 1, 0))
 
         do {
@@ -35,6 +38,37 @@ class Day13 : DayOf2016(13) {
         } while (!known.contains(target))
 
         return known[target].toString()
+    }
+
+    override fun second(data: String): String {
+        val fav = data.toInt()
+
+        val moves = listOf(
+                0 to 1,
+                0 to -1,
+                1 to 0,
+                -1 to 0
+        )
+
+        val known = mutableMapOf((1 to 1) to 0)
+        val queue = mutableListOf(Triple(1, 1, 0))
+
+        do {
+            val (x, y, step) = queue.removeAt(0)
+
+            val next = moves
+                    .map { x + it.first to y + it.second }
+                    .filter { it.first >= 0 && it.second >= 0 }
+                    .filter { isOpen(it.first, it.second, fav) }
+                    .filter { !known.contains(it) }
+
+            next.forEach {
+                known.put(it, step + 1)
+                queue.add(Triple(it.first, it.second, step + 1))
+            }
+        } while (queue.first().third <= 50)
+
+        return known.filterValues { it <= 50 }.size.toString()
     }
 
     fun isOpen(x: Int, y: Int, fav: Int) =
