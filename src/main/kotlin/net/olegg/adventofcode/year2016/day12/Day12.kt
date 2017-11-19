e172173b24085e502f6128b714ba186c3cc6815d
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-11-18 21:48:52 -0800

    solve 12th day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2016/day12/Day12.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2016/day12/Day12.kt
@@ -7,18 +7,32 @@ class Day12 : DayOf2016(12) {
     override fun first(data: String): String {
         val program = data.lines().filter { it.isNotBlank() }
         val registers = IntArray(4)
+
+        return eval(program, registers)[0].toString()
+    }
+
+    override fun second(data: String): String {
+        val program = data.lines().filter { it.isNotBlank() }
+        val registers = IntArray(4)
+        registers[register("c")] = 1
+
+        return eval(program, registers)[0].toString()
+    }
+
+    fun eval(program: List<String>, registers: IntArray): IntArray {
+        val output = registers.clone()
         var position = 0
         while (position in program.indices) {
             val parsed = program[position].split("\\s+".toRegex())
             position += when (parsed[0]) {
-                "cpy" -> { registers[register(parsed[2])] = value(parsed[1], registers); 1 }
-                "inc" -> { registers[register(parsed[1])] += 1; 1 }
-                "dec" -> { registers[register(parsed[1])] -= 1; 1 }
-                "jnz" -> { if (value(parsed[1], registers) == 0) 1 else value(parsed[2], registers) }
+                "cpy" -> { output[register(parsed[2])] = value(parsed[1], output); 1 }
+                "inc" -> { output[register(parsed[1])] += 1; 1 }
+                "dec" -> { output[register(parsed[1])] -= 1; 1 }
+                "jnz" -> { if (value(parsed[1], output) == 0) 1 else value(parsed[2], output) }
                 else -> throw RuntimeException("unimplemented")
             }
         }
-        return registers[0].toString()
+        return output
     }
 
     fun value(index: String, registers: IntArray) = when (index[0]) {
