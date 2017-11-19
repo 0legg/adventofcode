4b72cbd0ee1b828abb8122c602d5f094f1da53b9
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-11-19 14:10:43 -0800

    solve 20th day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2016/day20/Day20.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2016/day20/Day20.kt
@@ -11,27 +11,44 @@ class Day20 : DayOf2016(20) {
     val regex = "(\\d+)-(\\d+)".toRegex()
 
     override fun first(data: String): String {
+        val banned = createBlacklist(data)
+
+        return (if (banned.first().start > 0) 0 else banned.first().endInclusive + 1).toString()
+    }
+
+    override fun second(data: String): String {
+        val banned = createBlacklist(data)
+
+        return (banned.fold((-1L..-1L) to 0L) { acc, range ->
+                    range to acc.second + (range.start - acc.first.endInclusive - 1)
+                }.second +
+                ((1L shl 32) - banned.last().endInclusive - 1)).toString()
+    }
+
+    fun createBlacklist(data: String): TreeSet<LongRange> {
         val banned = TreeSet<LongRange>(compareBy( { it.start }, { it.endInclusive } ))
         data.lines()
                 .filter { it.isNotBlank() }
                 .mapNotNull { regex.find(it)?.groupValues?.let { it[1].toLong() .. it[2].toLong() } }
                 .forEach { mask ->
-                    var range = mask
-                    do {
-                        val start = banned.find { range.start in it || range.start - 1 in it }?.also { banned.remove(it) }
-                        val end = banned.find { range.endInclusive in it || range.endInclusive + 1 in it }?.also { banned.remove(it) }
+                    val join = banned.filter { mask.overlaps(it) } + listOf(mask)
+                    banned.removeAll(join)
 
-                        range = listOf(start, range, end).filterNotNull().fold(LongRange.EMPTY) { acc, next ->
-                            if (acc.isEmpty()) next else minOf(acc.start, next.start)..maxOf(acc.endInclusive, next.endInclusive)
-                        }
-
-                    } while (start != null || end != null)
-
-                    banned.add(range)
+                    banned.add(join.fold(LongRange.EMPTY) { acc, next ->
+                        if (acc.isEmpty()) next else minOf(acc.start, next.start)..maxOf(acc.endInclusive, next.endInclusive)
+                    })
                 }
+        return banned
+    }
 
+    inline fun LongRange.extend(value: Long) = LongRange(start - value, endInclusive + value)
 
-        return (if (banned.first().start > 0) 0 else banned.first().endInclusive + 1).toString()
+    fun LongRange.overlaps(other: LongRange): Boolean {
+        return with(extend(1)) {
+            other.first in this || other.endInclusive in this
+        } || with(other.extend(1)) {
+            this@overlaps.first in this || this@overlaps.endInclusive in this
+        }
     }
 }
 
