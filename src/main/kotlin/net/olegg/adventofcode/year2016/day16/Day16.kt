73612162db758f33b138691a801b8bf9869d08c7
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-11-19 01:00:03 -0800

    solve 16th day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2016/day16/Day16.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2016/day16/Day16.kt
@@ -1,6 +1,7 @@
 package net.olegg.adventofcode.year2016.day16
 
 import net.olegg.adventofcode.someday.SomeDay
+import net.olegg.adventofcode.utils.chunks
 import net.olegg.adventofcode.year2016.DayOf2016
 
 /**
@@ -8,26 +9,29 @@ import net.olegg.adventofcode.year2016.DayOf2016
  */
 class Day16 : DayOf2016(16) {
     override fun first(data: String): String {
-        val curve = generateSequence(data) {
+        return checksum(data, 272)
+    }
+
+    override fun second(data: String): String {
+        return checksum(data, 35651584)
+    }
+
+    fun checksum(initial: String, length: Int): String {
+        val curve = generateSequence(initial) {
             it + "0" + it.reversed().replace('0', '2').replace('1', '0').replace('2', '1')
         }
-                .dropWhile { it.length <= 272 }
+                .dropWhile { it.length <= length }
                 .first()
-                .substring(0, 272)
+                .substring(0, length)
 
         return generateSequence(curve) {
-            it.foldIndexed(listOf<Char>()) { index, acc, c ->
-                if (index % 2 == 0) {
-                    acc + listOf(c)
-                } else {
-                    acc.subList(0, acc.size - 1) + listOf(when ("${acc.last()}$c") {
-                        "00", "11" -> '1'
-                        "01", "10" -> '0'
-                        else -> '2'
-                    })
-                }
-            }.joinToString(separator = "")
-        }.first { it.length % 2 == 1 }
+            it.asSequence()
+                    .chunks(2)
+                    .filter { it.isNotEmpty() }
+                    .joinTo(StringBuilder(), separator = "") { if (it[0] == it[1]) "1" else "0" }
+                    .toString()
+        }
+                .first { it.length % 2 == 1 }
     }
 }
 
