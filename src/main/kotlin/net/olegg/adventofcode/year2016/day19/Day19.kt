47651f1c561a0169e5b3d3c5eec604e4d572ac04
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-11-19 13:17:35 -0800

    solve 19th day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2016/day19/Day19.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2016/day19/Day19.kt
@@ -3,6 +3,7 @@ package net.olegg.adventofcode.year2016.day19
 import net.olegg.adventofcode.someday.SomeDay
 import net.olegg.adventofcode.year2016.DayOf2016
 import java.util.BitSet
+import java.util.LinkedList
 
 /**
  * @see <a href="http://adventofcode.com/2016/day/18">Year 2016, Day 18</a>
@@ -10,16 +11,39 @@ import java.util.BitSet
 class Day19 : DayOf2016(19) {
     override fun first(data: String): String {
         val count = data.toInt()
-        val elfs = BitSet(count)
-        elfs.set(0, count)
+        val elves = BitSet(count)
+        elves.set(0, count)
         var position = 0
         repeat(count - 1) {
-            val next = maxOf(elfs.nextSetBit(position + 1), elfs.nextSetBit(0))
-            elfs.clear(next)
-            position = maxOf(elfs.nextSetBit(next + 1), elfs.nextSetBit(0))
+            val next = maxOf(elves.nextSetBit(position + 1), elves.nextSetBit(0))
+            elves.clear(next)
+            position = maxOf(elves.nextSetBit(next + 1), elves.nextSetBit(0))
         }
 
         return (position + 1).toString()
+    }
+
+    override fun second(data: String): String {
+        val count = data.toInt()
+        val elves = LinkedList((1 .. count).toList())
+
+        var target = elves.listIterator(count / 2)
+        target.next()
+        for (i in count downTo 2) {
+            target.remove()
+            repeat(i % 2 + 1) {
+                if (!target.hasNext()) {
+                    target = elves.listIterator()
+                }
+                target.next()
+            }
+        }
+
+        if (!target.hasNext()) {
+            target = elves.listIterator()
+        }
+
+        return (target.next()).toString()
     }
 }
 
