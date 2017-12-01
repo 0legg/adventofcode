83b10d5d27ca56d0d1a60022f641bf3bf516b633
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-11-30 21:03:13 -0800

    solve 1st day 1st task

---
+++ src/main/kotlin/net/olegg/adventofcode/year2017/day1/Day1.kt
@@ -0,0 +1,21 @@
+package net.olegg.adventofcode.year2017.day1
+
+import net.olegg.adventofcode.someday.SomeDay
+import net.olegg.adventofcode.year2017.DayOf2017
+
+/**
+ * @see <a href="http://adventofcode.com/2017/day/1">Year 2017, Day 1</a>
+ */
+class Day1 : DayOf2017(1) {
+    override fun first(data: String): String {
+        return data.trim()
+                .let { it + it[0] }
+                .windowed(2)
+                .filter { it[0] == it[1] }
+                .map { Character.digit(it[0], 10) }
+                .sum()
+                .toString()
+    }
+}
+
+fun main(args: Array<String>) = SomeDay.mainify(Day1::class)
