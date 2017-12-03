abaed666cb14ba95748f9ee554cf8b1d741500f0
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-12-02 21:32:34 -0800

    solve 3rd day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2017/day3/Day3.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2017/day3/Day3.kt
@@ -17,6 +17,45 @@ class Day3 : DayOf2017(3) {
 
         return ((square / 2) + abs(diff - (square / 2 - 1))).toString()
     }
+
+    override fun second(data: String): String {
+        val visited = mutableMapOf((0 to 0) to 1)
+        val input = data.trim().toInt()
+
+        var current = (0 to 0) to 1
+        var square = 0
+        while (current.second < input) {
+            val position = when {
+                current.first.second == -square  -> {
+                    current.first.first + 1 to current.first.second
+                }
+                current.first.first == square && current.first.second < square -> {
+                    current.first.first to current.first.second + 1
+                }
+                current.first.second == square && current.first.first > -square -> {
+                    current.first.first - 1 to current.first.second
+                }
+                current.first.first == -square && current.first.second > -square -> {
+                    current.first.first to current.first.second - 1
+                }
+                else -> {
+                    current.first.first + 1 to current.first.second
+                }
+            }
+
+            if (abs(position.first) > square || abs(position.second) > square) {
+                square += 1
+            }
+
+            val value = (-1 .. 1).flatMap { x -> (-1 .. 1).map { y -> x to y } }
+                    .mapNotNull { visited[position.first + it.first to position.second + it.second] }
+                    .sum()
+            current = position to value
+            visited += current
+        }
+
+        return current.second.toString()
+    }
 }
 
 fun main(args: Array<String>) = SomeDay.mainify(Day3::class)
