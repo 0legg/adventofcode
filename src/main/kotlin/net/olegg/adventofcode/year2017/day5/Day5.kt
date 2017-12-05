d426b3cbcd673ffe9f9866fd5fa14f399cb1a771
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-12-04 21:03:40 -0800

    solve 5th day 1st task

---
+++ src/main/kotlin/net/olegg/adventofcode/year2017/day5/Day5.kt
@@ -0,0 +1,29 @@
+package net.olegg.adventofcode.year2017.day5
+
+import net.olegg.adventofcode.someday.SomeDay
+import net.olegg.adventofcode.utils.scan
+import net.olegg.adventofcode.year2017.DayOf2017
+
+/**
+ * @see <a href="http://adventofcode.com/2017/day/5">Year 2017, Day 5</a>
+ */
+class Day5 : DayOf2017(5) {
+    override fun first(data: String): String {
+        val values = data.lines()
+                .map { it.toInt() }
+                .toTypedArray()
+
+        return generateSequence(0) { it + 1 }.scan(Triple(values, 0, 0)) { acc, _ ->
+            Triple(
+                    acc.first.copyOf().also { it[acc.second] += 1 },
+                    acc.second + acc.first[acc.second],
+                    acc.third + 1
+            )
+        }
+                .first { it.second !in values.indices }
+                .third
+                .toString()
+    }
+}
+
+fun main(args: Array<String>) = SomeDay.mainify(Day5::class)
