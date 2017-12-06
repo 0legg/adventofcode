479641c0032cdaebcca8d2d3521bef1bc10d0fac
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-12-05 21:15:25 -0800

    solve 6th day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2017/day6/Day6.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2017/day6/Day6.kt
@@ -32,6 +32,32 @@ class Day6 : DayOf2017(6) {
         }
         return steps.toString()
     }
+
+    override fun second(data: String): String {
+        val blocks = data.split("\\s+".toRegex())
+                .map { it.toInt() }
+
+        val seen = mutableMapOf<List<Int>, Int>()
+        var curr = blocks
+        var steps = 0
+        while (curr !in seen) {
+            seen.put(curr, steps)
+            steps += 1
+            val max = curr.max() ?: 0
+            val position = curr.indexOfFirst { it == max }
+
+            curr = curr.mapIndexed { index, value ->
+                val add = max / curr.size
+                val bonus = max % curr.size
+
+                return@mapIndexed add +
+                        (if (index == position) 0 else value) +
+                        (if (index in (position + 1)..(position + bonus)) 1 else 0) +
+                        (if ((position + bonus >= curr.size) && (index in 0..(position + bonus) % curr.size)) 1 else 0)
+            }
+        }
+        return (steps - (seen[curr] ?: 0)).toString()
+    }
 }
 
 fun main(args: Array<String>) = SomeDay.mainify(Day6::class)
