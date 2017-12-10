44b854ddabcd93644e05c60ebeb4e7f3ae4aaa82
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-12-09 21:36:36 -0800

    solve 10th day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2017/day10/Day10.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2017/day10/Day10.kt
@@ -21,6 +21,25 @@ class Day10 : DayOf2017(10) {
                 .let { it[0] * it[1] }
                 .toString()
     }
+
+    override fun second(data: String): String {
+        return data.trimIndent()
+                .map { it.toInt() }
+                .let { it + listOf(17, 31, 73, 47, 23) }
+                .let { list -> (0 until 64).fold(emptyList<Int>()) { acc, _ -> acc + list } }
+                .foldIndexed(List(256) { it } to 0) { index, acc, value ->
+                    val prev = acc.first + acc.first
+                    val curr = prev.subList(0, acc.second) + prev.subList(acc.second, acc.second + value).reversed() + prev.subList(acc.second + value, prev.size)
+                    val next = (curr.subList(acc.first.size, acc.first.size + acc.second) + curr.subList(acc.second, acc.first.size))
+                    return@foldIndexed next to ((acc.second + value + index) % acc.first.size)
+                }
+                .first
+                .chunked(16) {
+                    it.fold(0) { acc, value -> acc xor value }
+                }
+                .map { "${(it / 16).toString(16)}${(it % 16).toString(16)}" }
+                .joinToString(separator = "")
+    }
 }
 
 fun main(args: Array<String>) = SomeDay.mainify(Day10::class)
