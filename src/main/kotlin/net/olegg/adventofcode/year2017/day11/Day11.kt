96df51b2960795aa38544bb409ab4fc163d25f77
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-12-10 21:13:40 -0800

    solve 11th day 1st task

---
+++ src/main/kotlin/net/olegg/adventofcode/year2017/day11/Day11.kt
@@ -0,0 +1,32 @@
+package net.olegg.adventofcode.year2017.day11
+
+import net.olegg.adventofcode.someday.SomeDay
+import net.olegg.adventofcode.year2017.DayOf2017
+import kotlin.math.abs
+
+/**
+ * @see <a href="http://adventofcode.com/2017/day/11">Year 2017, Day 11</a>
+ */
+class Day11 : DayOf2017(11) {
+    override fun first(data: String): String {
+        return data.trimIndent()
+                .split(",")
+                .fold(0 to 0) { acc, value ->
+                    when (value) {
+                        "nw" -> acc.first - 1 to acc.second
+                        "n" -> acc.first to acc.second + 1
+                        "ne" -> acc.first + 1 to acc.second + 1
+                        "sw" -> acc.first - 1 to acc.second - 1
+                        "s" -> acc.first to acc.second - 1
+                        "se" -> acc.first + 1 to acc.second
+                        else -> acc
+                    }
+                }
+                .let {
+                    listOf(abs(it.first), abs(it.second), abs(it.first - it.second)).max()
+                }
+                .toString()
+    }
+}
+
+fun main(args: Array<String>) = SomeDay.mainify(Day11::class)
