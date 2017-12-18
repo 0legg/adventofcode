b158d1fa56e3b7995f17d3fa4189d910b0c80045
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-12-18 02:40:00 -0800

    solve 18th day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2017/day18/Day18.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2017/day18/Day18.kt
@@ -2,6 +2,8 @@ package net.olegg.adventofcode.year2017.day18
 
 import net.olegg.adventofcode.someday.SomeDay
 import net.olegg.adventofcode.year2017.DayOf2017
+import java.util.ArrayDeque
+import java.util.Queue
 
 /**
  * @see <a href="http://adventofcode.com/2017/day/18">Year 2017, Day 18</a>
@@ -31,6 +33,53 @@ class Day18 : DayOf2017(18) {
         }
 
         return "-1"
+    }
+
+    override fun second(data: String): String {
+        val regs = Array(2) { mutableMapOf("p" to it.toLong()) }
+        val stacks = Array(2) { ArrayDeque<Long>() }
+        val position = Array(2) { 0 }
+        var active = 0
+        val send = Array(2) { 0L }
+        val locked = Array(2) { false }
+        val stopped = Array(2) { false }
+        val ops = data.trimIndent()
+                .lines()
+                .map { it.split("\\s+".toRegex()).toList() }
+
+        while (!locked[active] && !stopped[active]) {
+            if (position[active] !in ops.indices) {
+                stopped[active] = true
+                active = 1 - active
+                continue
+            }
+
+            val op = ops[position[active]]
+            position[active] += when (op[0]) {
+                "snd" -> {
+                    stacks[1 - active].add(extract(regs[active], op[1]))
+                    locked[1 - active] = false
+                    send[active] = send[active] + 1
+                    1
+                }
+                "set" -> { regs[active][op[1]] = extract(regs[active], op[2]); 1 }
+                "add" -> { regs[active][op[1]] = extract(regs[active], op[1]) + extract(regs[active], op[2]); 1 }
+                "mul" -> { regs[active][op[1]] = extract(regs[active], op[1]) * extract(regs[active], op[2]); 1 }
+                "mod" -> { regs[active][op[1]] = (extract(regs[active], op[1]) % extract(regs[active], op[2]) + extract(regs[active], op[2])) % extract(regs[active], op[2]); 1 }
+                "rcv" -> if (stacks[active].isNotEmpty()) {
+                    regs[active][op[1]] = stacks[active].pollFirst()
+                    1
+                } else {
+                    locked[active] = true
+                    active = 1 - active
+                    0
+                }
+                "jgz" -> { if (extract(regs[active], op[1]) > 0L) extract(regs[active], op[2]).toInt() else 1 }
+                else -> 1
+            }
+        }
+
+        return send[1].toString()
     }
 
     private fun extract(map: Map<String, Long>, field: String): Long {
