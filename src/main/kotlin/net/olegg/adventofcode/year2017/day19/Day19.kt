45af3b1041896b35051c7c9d8c4877253f947eb7
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-12-18 21:36:58 -0800

    solve 19th day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2017/day19/Day19.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2017/day19/Day19.kt
@@ -42,6 +42,37 @@ class Day19 : DayOf2017(19) {
         return result.toString()
     }
 
+    override fun second(data: String): String {
+        val vector = listOf(
+                -1 to 0,
+                1 to 0,
+                0 to -1,
+                0 to 1
+        )
+        val map = data
+                .lines()
+                .map { it.toList() }
+
+        var pos = map[0].indexOfFirst { it == '|' } to 0
+        var dir = 0 to 1
+        var steps = 0
+        while (fit(map, pos)) {
+            steps += 1
+            val dirs = listOf(dir) + vector.filter { abs(it.first) != abs(dir.first) || abs(it.second) != abs(dir.second) }
+            val newDir = dirs.firstOrNull {
+                val next = pos.first + it.first to pos.second + it.second
+                fit(map, next) && map[next.second][next.first] != ' '
+            }
+            if (newDir != null) {
+                dir = newDir
+                pos = pos.first + dir.first to pos.second + dir.second
+            } else {
+                pos = -1 to -1
+            }
+        }
+        return steps.toString()
+    }
+
     fun fit(map: List<List<Any>>, pos: Pair<Int, Int>) = pos.second in map.indices && pos.first in map[pos.second].indices
 }
 
