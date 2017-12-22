69964052105208639114278eb6169d381368e9de
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-12-21 21:21:24 -0800

    solve 22nd day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2017/day22/Day22.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2017/day22/Day22.kt
@@ -34,6 +34,40 @@ class Day22 : DayOf2017(22) {
                     if (curr) acc.third else acc.third + 1)
         }.third.toString()
     }
+
+    override fun second(data: String): String {
+        val vectors = listOf(
+                -1 to 0,
+                0 to 1,
+                1 to 0,
+                0 to -1
+        )
+
+        val map = mutableMapOf<Pair<Int, Int>, Int>().apply {
+            val raw = data.trim().lines().map { it.map { ".W#F".indexOf(it) } }
+            raw.forEachIndexed { y, row ->
+                row.forEachIndexed { x, value ->
+                    put(y - raw.size / 2 to x - raw.size / 2, value)
+                }
+            }
+        }
+
+        return (0 until 10000000).fold(Triple((0 to 0), 0, 0)) { acc, _ ->
+            val curr = map.getOrDefault(acc.first, 0)
+            val dir = when (curr) {
+                0 -> acc.second + (vectors.size - 1)
+                1 -> acc.second
+                2 -> acc.second + 1
+                3 -> acc.second + (vectors.size / 2)
+                else -> acc.second
+            } % vectors.size
+            map.put(acc.first, (curr + 1) % 4)
+            return@fold Triple(
+                    (acc.first.first + vectors[dir].first to acc.first.second + vectors[dir].second),
+                    dir,
+                    if (curr == 1) acc.third + 1 else acc.third)
+        }.third.toString()
+    }
 }
 
 fun main(args: Array<String>) = SomeDay.mainify(Day22::class)
