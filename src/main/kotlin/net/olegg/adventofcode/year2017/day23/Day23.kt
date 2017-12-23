c0acd9f3a87c4116d7566d42260b117eb14a2f58
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-12-22 22:29:28 -0800

    solve 23rd day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2017/day23/Day23.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2017/day23/Day23.kt
@@ -2,6 +2,7 @@ package net.olegg.adventofcode.year2017.day23
 
 import net.olegg.adventofcode.someday.SomeDay
 import net.olegg.adventofcode.year2017.DayOf2017
+import java.math.BigInteger
 
 /**
  * @see <a href="http://adventofcode.com/2017/day/23">Year 2017, Day 23</a>
@@ -13,7 +14,7 @@ class Day23 : DayOf2017(23) {
                 .map { it.split("\\s+".toRegex()).toList() }
 
         var position = 0
-        val regs = mutableMapOf<String, Long>()
+        val regs = mutableMapOf<String, BigInteger>()
         var muls = 0
 
         while (position in ops.indices) {
@@ -22,7 +23,7 @@ class Day23 : DayOf2017(23) {
                 "set" -> regs[op[1]] = extract(regs, op[2])
                 "sub" -> regs[op[1]] = extract(regs, op[1]) - extract(regs, op[2])
                 "mul" -> { regs[op[1]] = extract(regs, op[1]) * extract(regs, op[2]); muls += 1 }
-                "jnz" -> if (extract(regs, op[1]) != 0L) position += (extract(regs, op[2]) - 1).toInt()
+                "jnz" -> if (extract(regs, op[1]) != BigInteger.ZERO) position += (extract(regs, op[2]).toInt() - 1)
             }
             position += 1
         }
@@ -30,8 +31,30 @@ class Day23 : DayOf2017(23) {
         return muls.toString()
     }
 
-    private fun extract(map: Map<String, Long>, field: String): Long {
-        return field.toLongOrNull() ?: map.getOrDefault(field, 0L)
+    override fun second(data: String): String {
+        val ops = data.trimIndent()
+                .lines()
+                .map { it.split("\\s+".toRegex()).toList() }
+
+        var position = 0
+        val regs = mutableMapOf("a" to BigInteger.ONE)
+
+        while (position in ops.indices) {
+            val op = ops[position]
+            when (op[0]) {
+                "set" -> regs[op[1]] = extract(regs, op[2])
+                "sub" -> regs[op[1]] = extract(regs, op[1]) - extract(regs, op[2])
+                "mul" -> regs[op[1]] = extract(regs, op[1]) * extract(regs, op[2])
+                "jnz" -> if (extract(regs, op[1]) != BigInteger.ZERO) position += (extract(regs, op[2]).toInt() - 1)
+            }
+            position += 1
+        }
+
+        return extract(regs, "h").toString()
+    }
+
+    private fun extract(map: Map<String, BigInteger>, field: String): BigInteger {
+        return field.toBigIntegerOrNull() ?: map.getOrDefault(field, BigInteger.ZERO)
     }
 }
 
