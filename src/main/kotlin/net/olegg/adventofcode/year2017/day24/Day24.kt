78e07ed8a1c1278d90e6df7d40cc7266851ad6c3
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-12-24 17:05:06 -0800

    solve 24th day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2017/day24/Day24.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2017/day24/Day24.kt
@@ -47,6 +47,44 @@ class Day24 : DayOf2017(24) {
 
         return best.toString()
     }
+
+    override fun second(data: String): String {
+        val ports =  data.trim().lines()
+                .map { it.split("/") }
+                .mapIndexed { index, value -> Triple((value.min()?.toInt() ?: 0), (value.max()?.toInt() ?: 0), index) }
+
+        val queue = ArrayDeque(ports.filter { it.first == 0 || it.second == 0 }
+                .map { port ->
+                    val next = if (port.first == 0) port.second else port.first
+                    return@map Triple(next, (2 to next), BitSet(ports.size).also { it.set(port.third) })
+                }
+        )
+
+        val visited = queue.map { it.first to it.third }.toMutableSet()
+        var best = 0 to 0
+
+        while (queue.isNotEmpty()) {
+            val curr = queue.pop()
+            best = listOf(best, curr.second).maxWith(compareBy({ it.first }, { it.second })) ?: (0 to 0)
+            ports.filter { !curr.third[it.third] }
+                    .filter { it.first == curr.first || it.second == curr.first }
+                    .map { port ->
+                        val next = if (port.first == curr.first) port.second else port.first
+                        Triple(
+                                next,
+                                (curr.second.first + 1 to curr.second.second + port.first + port.second),
+                                BitSet(ports.size).also { it.or(curr.third) }.also { it.set(port.third) }
+                        )
+                    }
+                    .filterNot { (it.first to it.third) in visited }
+                    .forEach {
+                        visited.add(it.first to it.third)
+                        queue.push(it)
+                    }
+        }
+
+        return best.second.toString()
+    }
 }
 
 fun main(args: Array<String>) = SomeDay.mainify(Day24::class)
