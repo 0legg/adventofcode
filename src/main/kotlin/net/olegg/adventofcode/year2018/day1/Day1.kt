2f04ecd26175b832c829e37dfacc9694c895938d
Oleg Godovykh <olegg.lieangel@gmail.com>
2018-11-30 21:14:28 -0800

    solve 1st day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2018/day1/Day1.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2018/day1/Day1.kt
@@ -16,6 +16,25 @@ class Day1 : DayOf2018(1) {
                 .sum()
                 .toString()
     }
+
+    override fun second(data: String): String {
+        val shifts = data
+            .trim()
+            .lines()
+            .map { it.removePrefix("+") }
+            .map { it.toLong() }
+
+        (0..1_000_000_000).fold(0L to mutableSetOf(0L)) { (prev, history), i ->
+            val next = prev + shifts[i % shifts.size]
+            if (next in history) {
+                return "$next"
+            }
+            history.add(next)
+            return@fold (next to history)
+        }
+
+        return ""
+    }
 }
 
 fun main(args: Array<String>) = SomeDay.mainify(Day1::class)
