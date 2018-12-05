67b4c880e95695d8159c8fe0a9597007d6dda043
Oleg Godovykh <olegg.lieangel@gmail.com>
2018-12-04 21:12:49 -0800

    solve 5th day 1st task

---
+++ src/main/kotlin/net/olegg/adventofcode/year2018/day5/Day5.kt
@@ -0,0 +1,23 @@
+package net.olegg.adventofcode.year2018.day5
+
+import net.olegg.adventofcode.someday.SomeDay
+import net.olegg.adventofcode.year2018.DayOf2018
+
+/**
+ * @see <a href="http://adventofcode.com/2018/day/5">Year 2018, Day 5</a>
+ */
+class Day5 : DayOf2018(5) {
+    override fun first(data: String): Any? {
+        val bad = ('a'..'z').map { a -> "$a${a.toUpperCase()}" } +
+                ('A'..'Z').map { a -> "$a${a.toLowerCase()}" }
+        var curr = data.trim()
+        do {
+            val prev = curr
+            curr = bad.fold(curr) { acc, token -> acc.replace(token, "") }
+        } while (prev != curr)
+
+        return curr.length
+    }
+}
+
+fun main(args: Array<String>) = SomeDay.mainify(Day5::class)
