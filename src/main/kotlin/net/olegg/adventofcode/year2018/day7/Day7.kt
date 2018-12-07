aa68bb369b0962c08dcbf6d755a0f72a84e87968
Oleg Godovykh <olegg.lieangel@gmail.com>
2018-12-06 22:22:43 -0800

    solve 7th day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2018/day7/Day7.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2018/day7/Day7.kt
@@ -3,6 +3,8 @@ package net.olegg.adventofcode.year2018.day7
 import net.olegg.adventofcode.someday.SomeDay
 import net.olegg.adventofcode.year2018.DayOf2018
 import java.util.ArrayDeque
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * @see <a href="http://adventofcode.com/2018/day/7">Year 2018, Day 7</a>
@@ -43,6 +45,61 @@ class Day7 : DayOf2018(7) {
         }
 
         return answer.joinToString(separator = "")
+    }
+
+    override fun second(data: String): Any? {
+        val edges = data
+                .trim()
+                .lines()
+                .mapNotNull { line ->
+                    PATTERN.matchEntire(line)?.let { match ->
+                        val (a, b) = match.destructured
+                        return@mapNotNull a to b
+                    }
+                }
+
+        val vertices = edges.flatMap { it.toList() }.toMutableSet()
+
+        val neighbors = edges
+                .groupBy { it.first }
+                .mapValues { neighbors -> neighbors.value.map { it.second }.toSet() }
+                .toMutableMap()
+
+        val start = vertices
+                .map { it to 0 }
+                .toMap()
+                .toMutableMap()
+
+        val workers = IntArray(5) { 0 }
+        while (vertices.isNotEmpty()) {
+            val available = vertices
+                    .filter { v ->
+                        neighbors.none { v in it.value }
+                    }
+            val soonest = start
+                    .filter { it.key in available }
+                    .map { it.value }
+                    .min() ?: 0
+
+            val next = available
+                    .sorted()
+                    .first { start[it] == soonest }
+
+            val worker = workers.indexOfFirst { it == workers.min() }
+            val time = max(workers[worker], soonest) + 61 + (next[0] - 'A')
+            workers[worker] = time
+
+            vertices.remove(next)
+            neighbors
+                    .remove(next)
+                    .orEmpty()
+                    .filter { it in vertices }
+                    .forEach { v ->
+                        start[v] = max(start[v] ?: 0, time)
+                    }
+        }
+
+        return workers.max()
     }
 }
 
