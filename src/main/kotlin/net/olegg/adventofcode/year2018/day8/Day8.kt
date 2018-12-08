fb65cc2975f6a446b36039857bba9ff732870eb1
Oleg Godovykh <olegg.lieangel@gmail.com>
2018-12-07 21:45:13 -0800

    solve 8th day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2018/day8/Day8.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2018/day8/Day8.kt
@@ -18,11 +18,37 @@ class Day8 : DayOf2018(8) {
         return sumMetadata(ArrayDeque(numbers))
     }
 
+    override fun second(data: String): Any? {
+        val numbers = data
+                .trim()
+                .split("\\s+".toRegex())
+                .map { it.toInt() }
+
+        return sumValues(ArrayDeque(numbers))
+    }
+
     private fun sumMetadata(data: Deque<Int>): Int {
         val child = data.pop()
         val metadata = data.pop()
 
         return (0 until child).sumBy { sumMetadata(data) } + (0 until metadata).sumBy { data.pop() }
+    }
+
+    private fun sumValues(data: Deque<Int>): Int {
+        val child = data.pop()
+        val metadata = data.pop()
+
+        val childValues = (0 until child)
+                .map { it to sumValues(data) }
+                .toMap()
+
+        return (0 until metadata).sumBy {
+            if (child > 0) {
+                childValues[data.pop() - 1] ?: 0
+            } else {
+                data.pop()
+            }
+        }
     }
 }
 
