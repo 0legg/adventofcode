a0f204f5bd90e27bb88c2c929171804fb64effa2
Oleg Godovykh <olegg.lieangel@gmail.com>
2019-01-28 21:11:32 -0800

    solve 24th day 1st task

---
+++ src/main/kotlin/net/olegg/adventofcode/year2018/day24/Day24.kt
@@ -0,0 +1,115 @@
+package net.olegg.adventofcode.year2018.day24
+
+import net.olegg.adventofcode.someday.SomeDay
+import net.olegg.adventofcode.year2018.DayOf2018
+
+/**
+ * See [Year 2018, Day 24](https://adventofcode.com/2018/day/24)
+ */
+class Day24 : DayOf2018(24) {
+  override fun first(data: String): Any? {
+    val (immune, infection) = data
+        .trim()
+        .split("\n\n")
+        .map { it.lines().drop(1) }
+        .mapIndexed { system, group -> group.mapIndexedNotNull { index, line -> Group.from(line, index + 1, system) } }
+
+    var board = immune + infection
+    while (board.distinctBy { it.system }.size == 2) {
+      val attacks = mutableMapOf<Pair<Int, Int>, Group?>()
+
+      board
+          .sortedWith(
+              compareByDescending<Group>{ it.power() }
+                  .thenByDescending { it.initiative }
+          )
+          .forEach { group ->
+            val targets = board
+                .filter { it.system != group.system }
+                .filterNot { it in attacks.values }
+
+            targets
+                .map { it to group.damage(it) }
+                .filterNot { it.second == 0L }
+                .sortedWith(
+                    compareByDescending<Pair<Group, Long>> { it.second }
+                        .thenByDescending { it.first.power() }
+                        .thenByDescending { it.first.initiative }
+                )
+                .firstOrNull()
+                ?.let { attacks[group.system to group.index] = it.first }
+          }
+
+      board
+          .sortedByDescending { it.initiative }
+          .forEach { group ->
+            if (group.units > 0) {
+              attacks[group.system to group.index]?.let { target ->
+                target.units -= group.damage(target) / target.hit
+              }
+            }
+          }
+
+      board = board.filter { it.units > 0 }
+    }
+    return board.filter { it.units > 0 }.sumBy { it.units.toInt() }
+  }
+
+  data class Group(
+      var units: Long,
+      val hit: Long,
+      val weak: Set<String>,
+      val immune: Set<String>,
+      val attack: Long,
+      val type: String,
+      val initiative: Int,
+      val index: Int,
+      val system: Int
+  ) {
+    companion object {
+      private val PATTERN = ("(-?\\d+) units each with (-?\\d+) hit points" +
+          " ?\\(?([^)]*)\\)? with an attack that does (-?\\d+) (\\w+) damage at initiative (-?\\d+)").toRegex()
+
+      fun from(string: String, index: Int, system: Int): Group? {
+        return PATTERN.matchEntire(string)?.let { match ->
+          val (unitsRaw, hitRaw, specRaw, attackRaw, type, initiativeRaw) = match.destructured
+          val weak = mutableSetOf<String>()
+          val immune = mutableSetOf<String>()
+          specRaw
+              .takeIf { it.isNotBlank() }
+              ?.split("; ")
+              ?.forEach { spec ->
+                val (kind, typesRaw) = spec.split(" to ")
+                val types = typesRaw.split(", ")
+                when (kind) {
+                  "weak" -> weak += types
+                  "immune" -> immune += types
+                }
+              }
+
+          return@let Group(
+              units = unitsRaw.toLong(),
+              hit = hitRaw.toLong(),
+              weak = weak,
+              immune = immune,
+              attack = attackRaw.toLong(),
+              type = type,
+              initiative = initiativeRaw.toInt(),
+              index = index,
+              system = system
+          )
+        }
+      }
+    }
+
+    fun damage(to: Group) = when (type) {
+      in to.immune -> 0L
+      in to.weak -> power() * 2L
+      else -> power()
+    }
+
+    fun power() = units * attack
+  }
+}
+
+fun main() = SomeDay.mainify(Day24::class)
