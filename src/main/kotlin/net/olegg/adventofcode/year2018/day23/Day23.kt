a551554125fa6dd999da954589b12525f42a0000
Oleg Godovykh <olegg.lieangel@gmail.com>
2019-11-25 16:55:05 -0800

    solve 23rd day 2nd task

--- src/main/kotlin/net/olegg/adventofcode/year2018/day23/Day23.kt
+++ src/main/kotlin/net/olegg/adventofcode/year2018/day23/Day23.kt
@@ -2,6 +2,7 @@ package net.olegg.adventofcode.year2018.day23
 
 import net.olegg.adventofcode.someday.SomeDay
 import net.olegg.adventofcode.year2018.DayOf2018
+import java.util.PriorityQueue
 import kotlin.math.abs
 
 /**
@@ -28,6 +29,53 @@ class Day23 : DayOf2018(23) {
     return bots.count { strong.distance(it) <= strong.r }
   }
 
+  override fun second(data: String): Any? {
+    val bots = data
+        .trim()
+        .lines()
+        .mapNotNull { line ->
+          PATTERN.matchEntire(line)?.let { match ->
+            val (x, y, z, r) = match.groupValues.drop(1).map { it.toLong() }
+            return@mapNotNull Bot(x, y, z, r)
+          }
+        }
+
+    var best = bots.count { it.distance(0, 0, 0) <= it.r } to 0L
+    val minX = bots.map { it.x }.min() ?: 0
+    val maxX = bots.map { it.x }.max() ?: 0
+    val minY = bots.map { it.y }.min() ?: 0
+    val maxY = bots.map { it.y }.max() ?: 0
+    val minZ = bots.map { it.z }.min() ?: 0
+    val maxZ = bots.map { it.z }.max() ?: 0
+
+    val queue = PriorityQueue<Pair<Box, Int>>(compareBy({ -it.second }, { it.first.size }))
+    queue.add(Box(minX, minY, minZ, maxX, maxY, maxZ) to bots.count())
+
+    while (queue.isNotEmpty()) {
+      val (box, botsIn) = queue.poll()
+      if (botsIn < best.first) continue
+      val selected = bots.filter { it.distance(box) <= it.r }
+      if (box.size <= 32) {
+        box.points().forEach { (x, y, z) ->
+          val distance = abs(x) + abs(y) + abs(z)
+          val botsCount = selected.count { it.distance(x, y, z) <= it.r }
+          if (botsCount > best.first || (botsCount == best.first && distance < best.second)) {
+            best = botsCount to distance
+            queue.removeIf { it.second < botsCount }
+          }
+        }
+        continue
+      }
+      val splits = box.split()
+          .map { newBox -> newBox to selected.count { it.distance(newBox) <= it.r } }
+          .filter { it.second >= best.first }
+
+      queue.addAll(splits)
+    }
+
+    return best
+  }
+
   data class Bot(
       val x: Long,
       val y: Long,
@@ -36,6 +84,51 @@ class Day23 : DayOf2018(23) {
   ) {
     fun distance(other: Bot): Long {
       return abs(x - other.x) + abs(y - other.y) + abs(z - other.z)
+    }
+
+    fun distance(otherX: Long, otherY: Long, otherZ: Long): Long {
+      return abs(x - otherX) + abs(y - otherY) + abs(z - otherZ)
+    }
+
+    fun distance(box: Box): Long {
+      return distance(x.coerceIn(box.minX, box.maxX), y.coerceIn(box.minY, box.maxY), z.coerceIn(box.minZ, box.maxZ))
+    }
+  }
+
+  data class Box(
+      val minX: Long,
+      val minY: Long,
+      val minZ: Long,
+      val maxX: Long,
+      val maxY: Long,
+      val maxZ: Long
+  ) {
+    fun split(): List<Box> {
+      val midX = (minX + maxX) / 2
+      val midY = (minY + maxY) / 2
+      val midZ = (minZ + maxZ) / 2
+      return listOf(
+          Box(minX, minY, minZ, midX, midY, midZ),
+          Box(midX + 1, minY, minZ, maxX, midY, midZ),
+          Box(minX, midY + 1, minZ, midX, maxY, midZ),
+          Box(midX + 1, midY + 1, minZ, maxX, maxY, midZ),
+          Box(minX, minY, midZ + 1, midX, midY, maxZ),
+          Box(midX + 1, minY, midZ + 1, maxX, midY, maxZ),
+          Box(minX, midY + 1, midZ + 1, midX, maxY, maxZ),
+          Box(midX + 1, midY + 1, midZ + 1, maxX, maxY, maxZ)
+      )
+    }
+
+    val size = maxOf(maxX - minX, maxY - minY, maxZ - minZ)
+
+    fun points(): Sequence<Triple<Long, Long, Long>> {
+      return (minX..maxX).asSequence().flatMap { x ->
+        (minY..maxY).asSequence().flatMap { y ->
+          (minZ..maxZ).asSequence().map { z ->
+            Triple(x, y, z)
+          }
+        }
+      }
     }
   }
 }
