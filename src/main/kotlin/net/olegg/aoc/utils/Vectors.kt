7169becc9f6209560e0a83f1e5d14529c4766ed0
Oleg Godovykh <olegg.lieangel@gmail.com>
2019-11-30 19:31:14 -0800

    add vector class

---
+++ src/main/kotlin/net/olegg/aoc/utils/Vectors.kt
@@ -0,0 +1,41 @@
+package net.olegg.aoc.utils
+
+import net.olegg.aoc.utils.Directions.D
+import net.olegg.aoc.utils.Directions.DL
+import net.olegg.aoc.utils.Directions.DR
+import net.olegg.aoc.utils.Directions.L
+import net.olegg.aoc.utils.Directions.R
+import net.olegg.aoc.utils.Directions.U
+import net.olegg.aoc.utils.Directions.UL
+import net.olegg.aoc.utils.Directions.UR
+
+//region 2D operations
+data class Vector2D (
+    var x: Int = 0,
+    var y: Int = 0
+) {
+  operator fun plus(other: Vector2D) = Vector2D(x + other.x, y + other.y)
+  operator fun plusAssign(other: Vector2D) = run { x += other.x; y += other.y }
+  operator fun minus(other: Vector2D) = Vector2D(x - other.x, y - other.y)
+  operator fun minusAssign(other: Vector2D) = run { x -= other.x; y -= other.y }
+  operator fun unaryMinus() = Vector2D(-x, -y)
+  operator fun times(other: Int) = Vector2D(x * other, y * other)
+  operator fun timesAssign(other: Int) = run { x *= other; y *= other }
+}
+
+enum class Directions(val step: Vector2D) {
+  U(0, -1),
+  D(0, 1),
+  L(-1, 0),
+  R(1, 0),
+  UL(-1, -1),
+  UR(1, -1),
+  DL(-1, 1),
+  DR(1, 1);
+
+  constructor(x: Int, y: Int): this(Vector2D(x, y))
+}
+
+val Neighbors4 = listOf(U, D, L, R)
+val Neighbors8 = listOf(UL, U, UR, L, R, DL, D, DR)
+//endregion
--- src/main/kotlin/net/olegg/aoc/year2016/day2/Day2.kt
+++ src/main/kotlin/net/olegg/aoc/year2016/day2/Day2.kt
@@ -1,6 +1,8 @@
 package net.olegg.aoc.year2016.day2
 
 import net.olegg.aoc.someday.SomeDay
+import net.olegg.aoc.utils.Directions
+import net.olegg.aoc.utils.Vector2D
 import net.olegg.aoc.year2016.DayOf2016
 
 /**
@@ -24,37 +26,23 @@ object Day2 : DayOf2016(2) {
       "000D000",
       "0000000"
   )
-
-  private val MOVES = mapOf(
-      'U' to (0 to -1),
-      'D' to (0 to 1),
-      'L' to (-1 to 0),
-      'R' to (1 to 0)
-  )
-
+  
   override fun first(data: String): Any? {
-    return data.lines().fold(Triple("", 2, 2)) { triple, command ->
-      val point = command.toCharArray().fold(triple.second to triple.third) { pair, symbol ->
-        (MOVES[symbol] ?: (0 to 0)).let {
-          Pair(pair.first + it.first, pair.second + it.second)
-        }.let {
-          if (KEYPAD1[it.first][it.second] != '0') it else pair
-        }
-      }
-      Triple(triple.first + "${KEYPAD1[point.second][point.first]}", point.first, point.second)
-    }.first
+    return solve(data.trim().lines(), KEYPAD1, Vector2D(2, 2))
   }
 
   override fun second(data: String): Any? {
-    return data.lines().fold(Triple("", 2, 4)) { triple, command ->
-      val point = command.toCharArray().fold(triple.second to triple.third) { pair, symbol ->
-        (MOVES[symbol] ?: (0 to 0)).let {
-          Pair(pair.first + it.first, pair.second + it.second)
-        }.let {
-          if (KEYPAD2[it.first][it.second] != '0') it else pair
+    return solve(data.trim().lines(), KEYPAD2, Vector2D(2, 4))
+  }
+
+  private fun solve(commands: List<String>, keypad: List<String>, start: Vector2D): String {
+    return commands.fold("" to start) { (acc, position), command ->
+      val point = command.toCharArray().fold(position) { accPosition, symbol ->
+        (accPosition + Directions.valueOf(symbol.toString()).step).let {
+          if (keypad[it.y][it.x] != '0') it else accPosition
         }
       }
-      Triple(triple.first + "${KEYPAD2[point.second][point.first]}", point.first, point.second)
+      acc + "${keypad[point.y][point.x]}" to point
     }.first
   }
 }
