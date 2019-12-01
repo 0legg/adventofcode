ea3fc75e7a3d4fb88081f0090943b455c51c2e36
Oleg Godovykh <olegg.lieangel@gmail.com>
2019-11-30 21:10:52 -0800

    solve 1st day 2nd task

--- src/main/kotlin/net/olegg/aoc/year2019/day1/Day1.kt
+++ src/main/kotlin/net/olegg/aoc/year2019/day1/Day1.kt
@@ -15,6 +15,20 @@ object Day1 : DayOf2019(1) {
         .map { (it / 3) - 2 }
         .sum()
   }
+
+  override fun second(data: String): Any? {
+    return data
+        .trim()
+        .lines()
+        .map { it.toInt() }
+        .map { mass ->
+          generateSequence(mass) { (it / 3) - 2 }
+              .drop(1)
+              .takeWhile { it > 0 }
+              .sum()
+        }
+        .sum()
+  }
 }
 
 fun main() = SomeDay.mainify(Day1)
