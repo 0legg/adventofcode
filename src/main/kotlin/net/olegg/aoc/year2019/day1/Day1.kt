1955e176c5ccc0cbe03727b2be67efa8c08757df
Oleg Godovykh <olegg.lieangel@gmail.com>
2019-11-30 21:07:56 -0800

    solve 1st day 1st task

---
+++ src/main/kotlin/net/olegg/aoc/year2019/day1/Day1.kt
@@ -0,0 +1,20 @@
+package net.olegg.aoc.year2019.day1
+
+import net.olegg.aoc.someday.SomeDay
+import net.olegg.aoc.year2019.DayOf2019
+
+/**
+ * See [Year 2019, Day 1](https://adventofcode.com/2019/day/1)
+ */
+object Day1 : DayOf2019(1) {
+  override fun first(data: String): Any? {
+    return data
+        .trim()
+        .lines()
+        .map { it.toInt() }
+        .map { (it / 3) - 2 }
+        .sum()
+  }
+}
+
+fun main() = SomeDay.mainify(Day1)
