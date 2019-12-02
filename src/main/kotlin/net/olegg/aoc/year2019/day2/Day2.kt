41b53c6ddca1e084f4ff50c2035154904f8145a4
Oleg Godovykh <olegg.lieangel@gmail.com>
2019-12-01 22:16:03 -0800

    solve 2nd day 2nd task

--- src/main/kotlin/net/olegg/aoc/year2019/day2/Day2.kt
+++ src/main/kotlin/net/olegg/aoc/year2019/day2/Day2.kt
@@ -15,19 +15,44 @@ object Day2 : DayOf2019(2) {
         .toIntArray()
     program[1] = 12
     program[2] = 2
+    val output = runProgram(program)
 
+    return output[0]
+  }
+
+  override fun second(data: String): Any? {
+    val program = data
+        .trim()
+        .parseInts(",")
+        .toIntArray()
+
+    for (noun in 0..99) {
+      for (verb in 0..99) {
+        program[1] = noun
+        program[2] = verb
+        if (runProgram(program)[0] == 19690720) {
+          return noun * 100 + verb
+        }
+      }
+    }
+
+    return -1
+  }
+
+  private fun runProgram(input: IntArray): IntArray {
+    val program = input.copyOf()
     var position = 0
     while (position in program.indices) {
       when (program[position]) {
         1 -> program[program[position + 3]] = program[program[position + 1]] + program[program[position + 2]]
         2 -> program[program[position + 3]] = program[program[position + 1]] * program[program[position + 2]]
-        99 -> return program[0]
+        99 -> return program
         else -> throw IllegalStateException()
       }
       position += 4
     }
 
-    return 0
+    return IntArray(0)
   }
 }
 
