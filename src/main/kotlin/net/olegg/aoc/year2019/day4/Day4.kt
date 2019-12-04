9c86969dd5428c403a7e1f0c5afcd933ce81c87f
Oleg Godovykh <olegg.lieangel@gmail.com>
2019-12-03 20:35:10 -0800

    solve 4th day 1st task

---
+++ src/main/kotlin/net/olegg/aoc/year2019/day4/Day4.kt
@@ -0,0 +1,25 @@
+package net.olegg.aoc.year2019.day4
+
+import net.olegg.aoc.someday.SomeDay
+import net.olegg.aoc.utils.parseInts
+import net.olegg.aoc.year2019.DayOf2019
+
+/**
+ * See [Year 2019, Day 4](https://adventofcode.com/2019/day/4)
+ */
+object Day4 : DayOf2019(4) {
+  override fun first(data: String): Any? {
+    val (from, to) = data
+        .trim()
+        .parseInts("-")
+
+    return (from..to)
+        .map { it.toString() }
+        .filter { it.length == 6 }
+        .filter { value -> value.windowed(2).any { it[0] == it[1] } }
+        .filter { value -> value.windowed(2).none { it[0] > it[1] } }
+        .count()
+  }
+}
+
+fun main() = SomeDay.mainify(Day4)
