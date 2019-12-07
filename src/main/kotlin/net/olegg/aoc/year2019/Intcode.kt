1711a516840103be8c57cbc6e1a368816a0dad8c
Oleg Godovykh <olegg.lieangel@gmail.com>
2019-12-06 20:29:08 -0800

    use buffers for memory and modes

--- src/main/kotlin/net/olegg/aoc/year2019/Intcode.kt
+++ src/main/kotlin/net/olegg/aoc/year2019/Intcode.kt
@@ -1,18 +1,19 @@
 package net.olegg.aoc.year2019
 
+import java.nio.CharBuffer
+import java.nio.IntBuffer
 import java.util.ArrayDeque
 import java.util.Deque
 
 object Intcode {
   fun eval(program: IntArray, inputs: List<Int> = emptyList()): List<Int> {
+    val memory = IntBuffer.wrap(program)
     val input = ArrayDeque(inputs)
     val output = ArrayDeque<Int>()
-    var position = 0
-    while (position in program.indices) {
-      val rawOp = program[position]
+    while (memory.hasRemaining()) {
+      val rawOp = memory.get()
       parseOp(rawOp)?.let { (op, modes) ->
-        val jump = op.eval(program, position, modes, input, output)
-        position = jump ?: position + op.length
+        op.eval(op, memory, modes, input, output)
       } ?: when (rawOp) {
         99 -> return output.toList()
         else -> throw UnsupportedOperationException("Opcode $rawOp is not supported")
@@ -24,63 +25,68 @@ object Intcode {
 
   data class Op(
       val length: Int,
-      val eval: (program: IntArray, position: Int, modes: List<Int>, input: Deque<Int>, output: Deque<Int>) -> Int?
-  )
+      val eval: Op.(memory: IntBuffer, modes: CharBuffer, input: Deque<Int>, output: Deque<Int>) -> Unit
+  ) {
+    fun parseArgs(memory: IntBuffer, modes: CharBuffer): List<Int> = modes.indices
+        .map { when (val mode = modes.get()) {
+          '0' -> memory[memory.get()]
+          '1' -> memory.get()
+          else -> throw IllegalArgumentException("Mode $mode is not supported")
+        } }
+  }
 
-  fun parseOp(rawOp: Int): Pair<Op, List<Int>>? {
+  fun parseOp(rawOp: Int): Pair<Op, CharBuffer>? {
     val opcode = rawOp % 100
     val op = OPS[opcode] ?: return null
     val rawModes = rawOp / 100
-    val modes = mutableListOf<Int>()
-    (1 until op.length).fold(rawModes) { curr, _ ->
-      modes += curr % 10
-      return@fold curr / 10
-    }
+    val modes = CharBuffer.wrap("$rawModes".padStart(op.length - 1, '0').reversed().toCharArray())
     return op to modes
   }
 
-  fun getValue(program: IntArray, value: Int, mode: Int) = when(mode) {
-    0 -> program[program[value]]
-    1 -> program[value]
-    else -> throw UnsupportedOperationException("Mode $mode is not supported")
-  }
-
   val OPS = mapOf(
-      1 to Op(4) { program, position, modes, _, _ ->
-        program[program[position + 3]] = getValue(program, position + 1, modes[0]) + getValue(program, position + 2, modes[1])
-        null
+      1 to Op(4) { memory, modes, _, _ ->
+        modes[2] = '1'
+        val (arg1, arg2, arg3) = parseArgs(memory, modes)
+        memory[arg3] = arg1 + arg2
       },
-      2 to Op(4) { program, position, modes, _, _ ->
-        program[program[position + 3]] = getValue(program, position + 1, modes[0]) * getValue(program, position + 2, modes[1])
-        null
+      2 to Op(4) { memory, modes, _, _ ->
+        modes[2] = '1'
+        val (arg1, arg2, arg3) = parseArgs(memory, modes)
+        memory[arg3] = arg1 * arg2
       },
-      3 to Op(2) { program, position, _, input, _ ->
-        program[program[position + 1]] = input.pop()
-        null
+      3 to Op(2) { memory, modes, input, _ ->
+        modes[0] = '1'
+        val (arg1) = parseArgs(memory, modes)
+        memory[arg1] = input.pop()
       },
-      4 to Op(2) { program, position, modes, _, output ->
-        output.offer(getValue(program, position + 1, modes[0]))
-        null
+      4 to Op(2) { memory, modes, _, output ->
+        val (arg1) = parseArgs(memory, modes)
+        output.offer(arg1)
       },
-      5 to Op(3) { program, position, modes, _, _ ->
-        getValue(program, position + 1, modes[0])
-            .takeIf { it != 0 }
-            ?.let { getValue(program, position + 2, modes[1]) }
+      5 to Op(3) { memory, modes, _, _ ->
+        val (arg1, arg2) = parseArgs(memory, modes)
+        if (arg1 != 0) {
+          memory.position(arg2)
+        }
       },
-      6 to Op(3) { program, position, modes, _, _ ->
-        getValue(program, position + 1, modes[0])
-            .takeIf { it == 0 }
-            ?.let { getValue(program, position + 2, modes[1]) }
+      6 to Op(3) { memory, modes, _, _ ->
+        val (arg1, arg2) = parseArgs(memory, modes)
+        if (arg1 == 0) {
+          memory.position(arg2)
+        }
       },
-      7 to Op(4) { program, position, modes, _, _ ->
-        program[program[position + 3]] =
-            if (getValue(program, position + 1, modes[0]) < getValue(program, position + 2, modes[1])) 1 else 0
-        null
+      7 to Op(4) { memory, modes, _, _ ->
+        modes[2] = '1'
+        val (arg1, arg2, arg3) = parseArgs(memory, modes)
+        memory[arg3] = if (arg1 < arg2) 1 else 0
       },
-      8 to Op(4) { program, position, modes, _, _ ->
-        program[program[position + 3]] =
-            if (getValue(program, position + 1, modes[0]) == getValue(program, position + 2, modes[1])) 1 else 0
-        null
+      8 to Op(4) { memory, modes, _, _ ->
+        modes[2] = '1'
+        val (arg1, arg2, arg3) = parseArgs(memory, modes)
+        memory[arg3] = if (arg1 == arg2) 1 else 0
       }
   )
+
+  private operator fun IntBuffer.set(index: Int, value: Int) = put(index, value)
+  private operator fun CharBuffer.set(index: Int, value: Char) = put(index, value)
 }
