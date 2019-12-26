703a68f6f20ad8e0108faff58862ecd81096e15e
Oleg Godovykh <olegg.lieangel@gmail.com>
2019-12-25 19:20:09 -0800

    solve 24th day 2nd task

--- src/main/kotlin/net/olegg/aoc/year2019/day24/Day24.kt
+++ src/main/kotlin/net/olegg/aoc/year2019/day24/Day24.kt
@@ -1,6 +1,10 @@
 package net.olegg.aoc.year2019.day24
 
 import net.olegg.aoc.someday.SomeDay
+import net.olegg.aoc.utils.Directions.D
+import net.olegg.aoc.utils.Directions.L
+import net.olegg.aoc.utils.Directions.R
+import net.olegg.aoc.utils.Directions.U
 import net.olegg.aoc.utils.Neighbors4
 import net.olegg.aoc.utils.Vector2D
 import net.olegg.aoc.year2019.DayOf2019
@@ -39,6 +43,62 @@ object Day24 : DayOf2019(24) {
         .replace('.', '0')
         .replace('#', '1')
         .toBigInteger(2)
+  }
+
+  override fun second(data: String): Any? {
+    val start = data
+        .trim()
+        .lines()
+        .map { it.toList() }
+
+    val size = start.size
+    val center = Vector2D(size / 2, size / 2)
+    val empty = List(size * size) { '.' }.chunked(size)
+
+    val result = (1..200).fold(mapOf(0 to start).withDefault { empty }) { acc, count ->
+      (-count..count).associateWith { level ->
+        val prev = acc.getValue(level)
+
+        return@associateWith prev.mapIndexed { y, row ->
+          row.mapIndexed { x, c ->
+            val base = Vector2D(x, y)
+            val neighbors = Neighbors4.map { it to it.step + base }
+                .flatMap { (dir, point) ->
+                  when {
+                    point == center -> when (dir) {
+                      L -> (0 until size).map { level - 1 to Vector2D(size - 1, it) }
+                      R -> (0 until size).map { level - 1 to Vector2D(0, it) }
+                      U -> (0 until size).map { level - 1 to Vector2D(it, size - 1) }
+                      D -> (0 until size).map { level - 1 to Vector2D(it, 0) }
+                      else -> emptyList()
+                    }
+                    point.x < 0 -> listOf(level + 1 to center + L.step)
+                    point.x >= size -> listOf(level + 1 to center + R.step)
+                    point.y < 0 -> listOf(level + 1 to center + U.step)
+                    point.y >= size -> listOf(level + 1 to center + D.step)
+                    else -> listOf(level to point)
+                  }
+                }
+                .count { (level, point) ->
+                  acc.getValue(level)[point] == '#'
+                }
+            when {
+              base == center -> '?'
+              c == '.' && neighbors in 1..2 -> '#'
+              c == '#' && neighbors != 1 -> '.'
+              else -> c
+            }
+          }
+        }
+      }.withDefault { empty }
+    }
+
+    return result.values
+        .sumBy { level ->
+          level.sumBy { row ->
+            row.count { it == '#' }
+          }
+        }
   }
 
   private fun List<List<Char>>.footprint() = joinToString("") { it.joinToString("") }
