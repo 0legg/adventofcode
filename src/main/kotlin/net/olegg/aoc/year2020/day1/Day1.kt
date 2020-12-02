afab5554166f198f2eae5f801d6b41fc29062435
Oleg Godovykh <0legg@users.noreply.github.com>
2020-12-01 20:42:52 -0800

    solve 1st day 2nd task

--- src/main/kotlin/net/olegg/aoc/year2020/day1/Day1.kt
+++ src/main/kotlin/net/olegg/aoc/year2020/day1/Day1.kt
@@ -15,6 +15,17 @@ object Day1 : DayOf2020(1) {
     val first = nums.first { (2020L - it) in nums }
     return first * (2020L - first)
   }
+
+  override fun second(data: String): Any? {
+    val nums = data
+        .parseLongs("\n")
+        .toSet()
+    val pairs = nums.flatMapIndexed { index, first ->
+      nums.drop(index + 1).map { first to it }
+    }
+    val pair = pairs.first { (first, second) -> (2020L - first - second) in nums }
+    return pair.first * pair.second * (2020L - pair.first - pair.second)
+  }
 }
 
 fun main() = SomeDay.mainify(Day1)
