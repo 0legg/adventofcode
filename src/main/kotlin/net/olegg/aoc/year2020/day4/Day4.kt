7793b268b13d84261157ab0e84eaa5bbe534eb1e
Oleg Godovykh <0legg@users.noreply.github.com>
2020-12-03 21:30:27 -0800

    solve 4th day 2nd task

--- src/main/kotlin/net/olegg/aoc/year2020/day4/Day4.kt
+++ src/main/kotlin/net/olegg/aoc/year2020/day4/Day4.kt
@@ -2,11 +2,16 @@ package net.olegg.aoc.year2020.day4
 
 import net.olegg.aoc.someday.SomeDay
 import net.olegg.aoc.year2020.DayOf2020
+import java.lang.Exception
 
 /**
  * See [Year 2020, Day 4](https://adventofcode.com/2020/day/4)
  */
 object Day4 : DayOf2020(4) {
+  private val HEIGHT_PATTERN = "^([0-9]+)(cm|in)$".toRegex()
+  private val COLOR_PATTERN = "^#[0-9a-f]{6}$".toRegex()
+  private val ID_PATTERN = "^[0-9]{9}$".toRegex()
+
   override fun first(data: String): Any? {
     val passports = data
       .trim()
@@ -16,7 +21,37 @@ object Day4 : DayOf2020(4) {
     return passports.count { (it - "cid").size == 7 }
   }
 
-  fun <T> List<T>.toPair(): Pair<T, T> = first() to last()
+  override fun second(data: String): Any? {
+    val passports = data
+      .trim()
+      .split("\n\n")
+      .map { it.replace("\n", " ") }
+      .map { line -> line.split(" ").associate { it.split(":").toPair() } }
+
+    return passports.count { (it - "cid").size == 7 && it.all { (key, value) -> isValid(key, value) } }
+  }
+
+  private fun isValid(field: String, value: String): Boolean {
+    return try {
+      when (field) {
+        "byr" -> value.toInt() in 1920..2002
+        "iyr" -> value.toInt() in 2010..2020
+        "eyr" -> value.toInt() in 2020..2030
+        "hgt" -> HEIGHT_PATTERN.find(value)?.destructured?.let { (length, measure) ->
+          (measure == "in" && length.toInt() in 59..76) or (measure == "cm" && length.toInt() in 150..193)
+        } ?: false
+        "hcl" -> COLOR_PATTERN.matches(value)
+        "ecl" -> value in setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+        "pid" -> ID_PATTERN.matches(value)
+        "cid" -> true
+        else -> false
+      }
+    } catch (_: Exception) {
+      false
+    }
+  }
+
+  private fun <T> List<T>.toPair(): Pair<T, T> = first() to last()
 }
 
 fun main() = SomeDay.mainify(Day4)
