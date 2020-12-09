4e5e2667185da20e48c54b7805c3a5f1d9d126d5
Oleg Godovykh <0legg@users.noreply.github.com>
2020-12-08 21:12:50 -0800

    solve 9th day 2nd task

--- src/main/kotlin/net/olegg/aoc/year2020/day9/Day9.kt
+++ src/main/kotlin/net/olegg/aoc/year2020/day9/Day9.kt
@@ -2,6 +2,7 @@ package net.olegg.aoc.year2020.day9
 
 import net.olegg.aoc.someday.SomeDay
 import net.olegg.aoc.utils.parseInts
+import net.olegg.aoc.utils.parseLongs
 import net.olegg.aoc.year2020.DayOf2020
 
 /**
@@ -11,13 +12,44 @@ object Day9 : DayOf2020(9) {
   override fun first(data: String): Any? {
     val nums = data
       .trim()
-      .parseInts(delimiters = "\n")
+      .parseLongs(delimiters = "\n")
 
     return nums.windowed(26)
       .map { it.take(25) to it.last() }
       .map { (head, tail) -> head.flatMapIndexed { i, x -> head.drop(i + 1).map { it + x } }.toSet() to tail }
       .first { (head, tail) -> tail !in head }
       .second
+  }
+
+  override fun second(data: String): Any? {
+    val nums = data
+      .trim()
+      .parseLongs(delimiters = "\n")
+
+    val bad = nums.windowed(26)
+      .map { it.take(25) to it.last() }
+      .map { (head, tail) -> head.flatMapIndexed { i, x -> head.drop(i + 1).map { it + x } }.toSet() to tail }
+      .first { (head, tail) -> tail !in head }
+      .second
+
+    val partials = nums.scan(0L) { acc, value -> acc + value }
+    var from = 0
+    var to = 2
+
+    while (to in partials.indices) {
+      val curr = partials[to] - partials[from]
+      when {
+        curr < bad -> to++
+        curr > bad -> {
+          from++
+          to = maxOf(to, from + 2)
+        }
+        else -> break
+      }
+    }
+
+    val subset = nums.subList(from, to)
+    return subset.minOf { it } + subset.maxOf { it }
   }
 }
 
