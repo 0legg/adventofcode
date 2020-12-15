e9db3dead49ef3a3f9c2fa687d042d64bc18de0c
Oleg Godovykh <0legg@users.noreply.github.com>
2020-12-14 21:16:38 -0800

    solve 15th day 2nd task

--- src/main/kotlin/net/olegg/aoc/year2020/day15/Day15.kt
+++ src/main/kotlin/net/olegg/aoc/year2020/day15/Day15.kt
@@ -11,13 +11,30 @@ object Day15 : DayOf2020(15) {
   override fun first(data: String): Any? {
     val nums = data.trim().parseInts(delimiters = ",")
 
-    val said = nums.toMutableList()
-    while (said.size < 2020) {
-      val lastMet = said.dropLast(1).lastIndexOf(said.last())
-      said += if (lastMet == -1) 0 else said.size - lastMet - 1
+    return solve(nums, 2020)
+  }
+
+  override fun second(data: String): Any? {
+    val nums = data.trim().parseInts(delimiters = ",")
+
+    return solve(nums, 30000000)
+  }
+
+  private fun solve(nums: List<Int>, steps: Int): Int {
+    val meets = nums.dropLast(1)
+      .mapIndexed { index, value -> value to index + 1 }
+      .toMap()
+      .toMutableMap()
+    var last = nums.last()
+    var pos = nums.size
+    while (pos < steps) {
+      val say = pos - (meets[last] ?: pos)
+      meets[last] = pos
+      last = say
+      pos++
     }
 
-    return said.last()
+    return last
   }
 }
 
