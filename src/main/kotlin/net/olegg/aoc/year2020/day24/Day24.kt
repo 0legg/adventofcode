11989b3bf228b0dc12ea5ce83bea5998a87029ac
Oleg Godovykh <0legg@users.noreply.github.com>
2020-12-23 21:31:28 -0800

    solve 24th day 2nd task

--- src/main/kotlin/net/olegg/aoc/year2020/day24/Day24.kt
+++ src/main/kotlin/net/olegg/aoc/year2020/day24/Day24.kt
@@ -31,6 +31,33 @@ object Day24 : DayOf2020(24) {
 
     return tiles.groupBy { it }.count { it.value.size % 2 == 1 }
   }
+
+  override fun second(data: String): Any? {
+    val items = data
+      .trim()
+      .lines()
+      .map { line ->
+        pattern.findAll(line).map { it.groupValues[1] }.toList()
+      }
+
+    val start = items
+      .map { steps ->
+        steps.fold(Vector3D()) { acc, value -> acc + dirs[value]!! }
+      }
+      .groupBy { it }
+      .filterValues { it.size % 2 == 1 }
+      .keys
+
+    val result = (0 until 100).fold(start) { prev, _ ->
+      val neighbors = prev.flatMap { cell -> dirs.values.map { it + cell } }
+        .groupBy { it }
+        .mapValues { it.value.size }
+
+      return@fold (prev.filter { neighbors[it] in 1..2 } + (neighbors.filterValues { it == 2 }.keys - prev)).toSet()
+    }
+
+    return result.size
+  }
 }
 
 fun main() = SomeDay.mainify(Day24)
