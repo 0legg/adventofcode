0c27e6fffeed72ac1bd185798656b1a6f6ca3303
Oleg Godovykh <0legg@users.noreply.github.com>
2021-12-03 21:23:15 -0800

    solve 4th day 2nd task

--- src/main/kotlin/net/olegg/aoc/year2021/day4/Day4.kt
+++ src/main/kotlin/net/olegg/aoc/year2021/day4/Day4.kt
@@ -43,6 +43,47 @@ object Day4 : DayOf2021(4) {
     return 0
   }
 
+  override fun second(data: String): Any? {
+    val values = data
+      .lines()
+      .first()
+      .parseInts(",")
+    val boards = data
+      .split("\n\n")
+      .drop(1)
+      .map { it.replace("\\s+".toRegex(), " ").parseInts(" ").map { value -> value to false } }
+
+    values.fold(boards) { acc, value ->
+      val newAcc = acc.map {
+        it.map { (curr, seen) -> if (curr == value) curr to true else curr to seen }
+      }
+
+      val filteredAcc = newAcc.filter { board ->
+        for (x in 0 until 5) {
+          if ((0 until 5).all { y -> board[5 * y + x].second }) {
+            return@filter false
+          }
+        }
+
+        for (y in 0 until 5) {
+          if ((0 until 5).all { x -> board[5 * y + x].second }) {
+            return@filter false
+          }
+        }
+
+        return@filter true
+      }
+
+      if (filteredAcc.isEmpty()) {
+        return score(newAcc.first(), value)
+      }
+
+      filteredAcc
+    }
+
+    return 0
+  }
+
   private fun score(board: List<Pair<Int, Boolean>>, value: Int) =
     board.filter { !it.second }.sumOf { it.first } * value
 }
