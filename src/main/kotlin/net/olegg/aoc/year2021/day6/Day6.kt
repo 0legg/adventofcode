6434539ec5746c1da906034c909e44f333599384
Oleg Godovykh <0legg@users.noreply.github.com>
2021-12-05 21:19:12 -0800

    solve 6th day 2nd task

--- src/main/kotlin/net/olegg/aoc/year2021/day6/Day6.kt
+++ src/main/kotlin/net/olegg/aoc/year2021/day6/Day6.kt
@@ -1,7 +1,7 @@
 package net.olegg.aoc.year2021.day6
 
 import net.olegg.aoc.someday.SomeDay
-import net.olegg.aoc.utils.parseInts
+import net.olegg.aoc.utils.parseLongs
 import net.olegg.aoc.year2021.DayOf2021
 
 /**
@@ -9,14 +9,24 @@ import net.olegg.aoc.year2021.DayOf2021
  */
 object Day6 : DayOf2021(6) {
   override fun first(data: String): Any? {
-    val fish = data.trim().parseInts(",")
-    val start = fish.groupBy { it }
-      .mapValues { it.value.size }
+    val fish = data.trim().parseLongs(",")
 
-    val end = (1 .. 80).fold(start) { acc, day ->
+    return solve(fish, 80)
+  }
+
+  override fun second(data: String): Any? {
+    val fish = data.trim().parseLongs(",")
+
+    return solve(fish, 256)
+  }
+
+  private fun solve(input: List<Long>, days: Int): Long {
+    val start = input.groupBy { it }
+      .mapValues { it.value.size.toLong() }
+    val end = (1..days).fold(start) { acc, _ ->
       val new = acc.mapKeys { it.key - 1 }.toMutableMap()
-      new[8] = new.getOrDefault(-1, 0)
-      new[6] = new.getOrDefault(-1, 0) + new.getOrDefault(6, 0)
+      new[8] = new.getOrDefault(-1, 0L)
+      new[6] = new.getOrDefault(-1, 0L) + new.getOrDefault(6, 0L)
       new.remove(-1)
       return@fold new.toMap()
     }
