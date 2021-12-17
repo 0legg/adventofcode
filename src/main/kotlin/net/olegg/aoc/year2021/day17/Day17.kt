39b843a39cb510536dee5e507dc387d356fe8897
Oleg Godovykh <0legg@users.noreply.github.com>
2021-12-16 19:11:12 -1000

    solve 17th day 1st task

---
+++ src/main/kotlin/net/olegg/aoc/year2021/day17/Day17.kt
@@ -0,0 +1,41 @@
+package net.olegg.aoc.year2021.day17
+
+import net.olegg.aoc.someday.SomeDay
+import net.olegg.aoc.utils.Vector2D
+import net.olegg.aoc.year2021.DayOf2021
+import kotlin.math.sign
+
+/**
+ * See [Year 2021, Day 17](https://adventofcode.com/2021/day/17)
+ */
+object Day17 : DayOf2021(17) {
+  private val REGEX = "target area: x=(-?\\d+)\\.\\.(-?\\d+), y=(-?\\d+)\\.\\.(-?\\d+)".toRegex()
+
+  override fun first(data: String): Any? {
+    val (fromX, toX, fromY, toY) = REGEX.find(data)?.groupValues.orEmpty().drop(1).map { it.toInt() }
+
+    var best = fromY
+    (0..toX).forEach { x ->
+      (-100..100).forEach { y ->
+        val positions = generateSequence(Vector2D(0, 0) to Vector2D(x, y)) { (pos, speed) ->
+          val nextPos = pos + speed
+          val nextSpeed = Vector2D(
+            x = speed.x - speed.x.sign,
+            y = speed.y - 1,
+          )
+          when {
+            speed.x == 0 && pos.y < fromY -> null
+            else -> nextPos to nextSpeed
+          }
+        }.map { it.first }.toList()
+
+        if (positions.any { it.x in fromX..toX && it.y in fromY..toY}) {
+          best = maxOf(best, positions.maxOf { it.y })
+        }
+      }
+    }
+    return best
+  }
+}
+
+fun main() = SomeDay.mainify(Day17)
