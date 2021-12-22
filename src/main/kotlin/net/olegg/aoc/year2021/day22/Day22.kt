62ff9ef68b0d55389d2e4b072c4be28779872555
Oleg Godovykh <0legg@users.noreply.github.com>
2021-12-21 22:05:10 -0800

    solve 22nd day 2nd task

--- src/main/kotlin/net/olegg/aoc/year2021/day22/Day22.kt
+++ src/main/kotlin/net/olegg/aoc/year2021/day22/Day22.kt
@@ -20,33 +20,99 @@ object Day22 : DayOf2021(22) {
           val op = parsed[1]
           val from = Vector3D(parsed[2].toInt(), parsed[4].toInt(), parsed[6].toInt())
           val to = Vector3D(parsed[3].toInt(), parsed[5].toInt(), parsed[7].toInt())
-          Triple(op, from, to)
+          op to (from to to)
         } else {
           null
         }
       }
 
-    return ops
-      .filterNot { (_, from, to) ->
-        from.toList().any { it >= 50 } || to.toList().any { it <= -50 }
-      }
-      .fold(setOf<Vector3D>()) { acc, (op, from, to) ->
-        val cube = (from.x..to.x).flatMap { x ->
-          (from.y..to.y).flatMap { y ->
-            (from.z..to.z).map { z ->
-              Vector3D(x, y, z)
-            }
-          }
-        }.toSet()
+    val basicCube = Vector3D(-50, -50, -50) to Vector3D(50, 50, 50)
 
-        when (op) {
-          "on" -> acc + cube
-          "off" -> acc - cube
-          else -> acc
+    return solve(ops.filter { (_, cube) -> intersects(cube, basicCube) })
+  }
+
+  override fun second(data: String): Any? {
+    val ops = data.trim().lines()
+      .mapNotNull { line ->
+        val parsed = pattern.find(line)?.groupValues.orEmpty()
+        if (parsed.size == 8) {
+          val op = parsed[1]
+          val from = Vector3D(parsed[2].toInt(), parsed[4].toInt(), parsed[6].toInt())
+          val to = Vector3D(parsed[3].toInt(), parsed[5].toInt(), parsed[7].toInt())
+          op to (from to to)
+        } else {
+          null
         }
       }
-      .filterNot { cubit -> cubit.toList().any { it.absoluteValue > 50 } }
-      .size
+
+    return solve(ops)
+  }
+
+  private fun intersects(
+    old: Pair<Vector3D, Vector3D>,
+    new: Pair<Vector3D, Vector3D>,
+  ): Boolean {
+    val (oldFrom, oldTo) = old
+    val (newFrom, newTo) = new
+
+    return !(oldTo.x < newFrom.x || oldTo.y < newFrom.y || oldTo.z < newFrom.z
+      || oldFrom.x > newTo.x || oldFrom.y > newTo.y || oldFrom.z > newTo.z)
+  }
+
+  private fun solve(ops: List<Pair<String, Pair<Vector3D, Vector3D>>>): Long {
+    val finalCubes = ops.fold(emptyList<Pair<Vector3D, Vector3D>>()) { acc, (op, cube) ->
+      acc.flatMap { oldCube ->
+        if (intersects(oldCube, cube)) {
+          split(oldCube, cube)
+        } else {
+          listOf(oldCube)
+        }
+      } + if (op == "on") listOf(cube) else emptyList()
+    }
+
+    return finalCubes.sumOf { cube ->
+      (cube.second - cube.first).toList().map { it + 1L }.reduce { a, b -> a * b }
+    }
+  }
+
+  private fun split(
+    old: Pair<Vector3D, Vector3D>,
+    new: Pair<Vector3D, Vector3D>,
+  ): List<Pair<Vector3D, Vector3D>> {
+    val (oldFrom, oldTo) = old
+    val (newFrom, newTo) = new
+
+    val xParts = buildParts(oldFrom.x, oldTo.x, newFrom.x, newTo.x)
+    val yParts = buildParts(oldFrom.y, oldTo.y, newFrom.y, newTo.y)
+    val zParts = buildParts(oldFrom.z, oldTo.z, newFrom.z, newTo.z)
+
+    return xParts.flatMap { (fromX, toX, partX) ->
+      yParts.flatMap { (fromY, toY, partY) ->
+        zParts.mapNotNull { (fromZ, toZ, partZ) ->
+          (Vector3D(fromX, fromY, fromZ) to Vector3D(toX, toY, toZ)).takeIf { !partX || !partY ||!partZ }
+        }
+      }
+    }
+  }
+
+  private fun buildParts(oldFrom: Int, oldTo: Int, newFrom: Int, newTo: Int): List<Triple<Int, Int, Boolean>> {
+    return buildList {
+      var partial = newFrom <= oldFrom
+      var mark = oldFrom
+      if (newFrom in ((mark + 1)..oldTo)) {
+        add(Triple(mark, newFrom - 1, partial))
+        mark = newFrom
+        partial = true
+      }
+      if (newTo in (mark..oldTo)) {
+        add(Triple(mark, newTo, partial))
+        mark = newTo + 1
+        partial = false
+      }
+      if (mark <= oldTo) {
+        add(Triple(mark, oldTo, partial))
+      }
+    }
   }
 }
 
