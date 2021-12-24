1d93737e7fa54d7e9e9af0f4c9c4d713f29cca13
Oleg Godovykh <0legg@users.noreply.github.com>
2021-12-23 23:22:21 -0800

    solve 24th day 1st task

---
+++ src/main/kotlin/net/olegg/aoc/year2021/day24/Day24.kt
@@ -0,0 +1,143 @@
+package net.olegg.aoc.year2021.day24
+
+import net.olegg.aoc.someday.SomeDay
+import net.olegg.aoc.year2021.DayOf2021
+
+/**
+ * See [Year 2021, Day 24](https://adventofcode.com/2021/day/24)
+ */
+object Day24 : DayOf2021(24) {
+  private val MAPPING = listOf('w', 'x', 'y', 'z').withIndex().associate { it.value to it.index }
+  private val DIGITS = 1..9
+
+  override fun first(data: String): Any? {
+    val rawOps = data.trim()
+      .split("inp w")
+      .filter { it.isNotBlank() }
+      .map { block ->
+        block.trim().lines().map { Op.parse(it) }
+      }
+
+    val pushOut = Op.Div(Arg.Reg(MAPPING['z']!!), Arg.Val(26))
+
+    val diffs = buildList {
+      val stack = ArrayDeque<Pair<Int, Int>>()
+      rawOps.forEachIndexed { index, block ->
+        if (pushOut in block) {
+          val diff = block.last { it is Op.Add && it.a == Arg.Reg(MAPPING['x']!!) } as Op.Add
+          val linked = stack.removeLast()
+          add(Triple(linked.first, index, linked.second + (diff.b as Arg.Val).value))
+        } else {
+          val diff = block.last { it is Op.Add && it.a == Arg.Reg(MAPPING['y']!!) } as Op.Add
+          stack.addLast(index to (diff.b as Arg.Val).value)
+        }
+      }
+    }
+
+    return diffs
+      .flatMap { (first, second, diff) ->
+        val match = DIGITS.filter { it in DIGITS && it + diff in DIGITS }
+          .maxOf { it }
+        listOf(first to match, second to match + diff)
+      }
+      .sortedBy { it.first }
+      .map { it.second }
+      .joinToString("")
+  }
+
+  sealed interface Op {
+    fun apply(regs: IntArray, input: IntArray, pos: Int)
+
+    data class Inp(
+      val reg: Int,
+    ) : Op {
+      override fun apply(regs: IntArray, input: IntArray, pos: Int) {
+        regs[reg] = input[pos]
+      }
+    }
+
+    data class Add(
+      val a: Arg,
+      val b: Arg,
+    ) : Op {
+      override fun apply(regs: IntArray, input: IntArray, pos: Int) {
+        regs[(a as Arg.Reg).reg] = a.getValue(regs) + b.getValue(regs)
+      }
+    }
+
+    data class Mul(
+      val a: Arg,
+      val b: Arg,
+    ) : Op {
+      override fun apply(regs: IntArray, input: IntArray, pos: Int) {
+        regs[(a as Arg.Reg).reg] = a.getValue(regs) * b.getValue(regs)
+      }
+    }
+
+    data class Div(
+      val a: Arg,
+      val b: Arg,
+    ) : Op {
+      override fun apply(regs: IntArray, input: IntArray, pos: Int) {
+        regs[(a as Arg.Reg).reg] = a.getValue(regs) / b.getValue(regs)
+      }
+    }
+
+    data class Mod(
+      val a: Arg,
+      val b: Arg,
+    ) : Op {
+      override fun apply(regs: IntArray, input: IntArray, pos: Int) {
+        regs[(a as Arg.Reg).reg] = ((a.getValue(regs) % b.getValue(regs)) + b.getValue(regs)) % b.getValue(regs)
+      }
+    }
+
+    data class Eql(
+      val a: Arg,
+      val b: Arg,
+    ) : Op {
+      override fun apply(regs: IntArray, input: IntArray, pos: Int) {
+        regs[(a as Arg.Reg).reg] = if (a.getValue(regs) == b.getValue(regs)) 1 else 0
+      }
+    }
+
+    companion object {
+      fun parse(raw: String): Op {
+        val tokens = raw.split(" ")
+        return when (tokens[0]) {
+          "inp" -> Inp(MAPPING[tokens[1].first()]!!)
+          "add" -> Add(parseArg(tokens[1]), parseArg(tokens[2]))
+          "mul" -> Mul(parseArg(tokens[1]), parseArg(tokens[2]))
+          "div" -> Div(parseArg(tokens[1]), parseArg(tokens[2]))
+          "mod" -> Mod(parseArg(tokens[1]), parseArg(tokens[2]))
+          "eql" -> Eql(parseArg(tokens[1]), parseArg(tokens[2]))
+          else -> error("Unknown operation \"$raw\"")
+        }
+      }
+
+      private fun parseArg(raw: String): Arg = try {
+        Arg.Val(raw.toInt())
+      } catch (_: Exception) {
+        Arg.Reg(MAPPING[raw.first()]!!)
+      }
+    }
+  }
+
+  sealed interface Arg {
+    fun getValue(regs: IntArray): Int
+
+    data class Val(
+      val value: Int
+    ) : Arg {
+      override fun getValue(regs: IntArray): Int = value
+    }
+
+    data class Reg(
+      val reg: Int
+    ) : Arg {
+      override fun getValue(regs: IntArray): Int = regs[reg]
+    }
+  }
+}
+
+fun main() = SomeDay.mainify(Day24)
