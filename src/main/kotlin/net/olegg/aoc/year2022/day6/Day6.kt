3e0514ac2c53759fbe3ec9191f15520688f5d7bc
Oleg Godovykh <0legg@users.noreply.github.com>
2022-12-05 21:04:35 -0800

    solve 6th day 2nd task

--- src/main/kotlin/net/olegg/aoc/year2022/day6/Day6.kt
+++ src/main/kotlin/net/olegg/aoc/year2022/day6/Day6.kt
@@ -9,9 +9,17 @@ import net.olegg.aoc.year2022.DayOf2022
 object Day6 : DayOf2022(6) {
 
   override fun first(): Any? {
+    return solve(4)
+  }
+
+  override fun second(): Any? {
+    return solve(14)
+  }
+
+  private fun solve(length: Int): Int {
     return data
-      .windowed(4)
-      .indexOfFirst { it.toSet().size == 4 } + 4
+      .windowed(length)
+      .indexOfFirst { it.toSet().size == length } + length
   }
 }
 
