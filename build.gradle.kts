ae4a3d6357213e7016113ec7914499f634bff032
Oleg Godovykh <0legg@users.noreply.github.com>
2022-12-05 20:57:10 -0800

    split years into separate gradle packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,56 +1,59 @@
 import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-  kotlin("jvm")
-  kotlin("plugin.serialization")
-  id("com.github.gmazzo.buildconfig")
-  id("io.gitlab.arturbosch.detekt")
-}
-
-group = "net.olegg.aoc"
-version = "2022.0.0"
-
-repositories {
-  mavenCentral()
-}
-
-buildConfig {
-  packageName(project.group.toString())
-  buildConfigField("String", "COOKIE", "\"${project.findProperty("COOKIE")?.toString() ?: "Please provide cookie"}\"")
-}
-
-tasks.withType<KotlinCompile> {
-  kotlinOptions {
-    jvmTarget = "14"
-    languageVersion = "1.8"
-    allWarningsAsErrors = true
-    freeCompilerArgs += listOf(
-      "-Xsuppress-version-warnings",
-      "-opt-in=kotlin.RequiresOptIn",
-      "-opt-in=kotlin.ExperimentalStdlibApi",
-      "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-      "-opt-in=kotlinx.coroutines.FlowPreview",
-    )
+buildscript {
+  repositories {
+    mavenCentral()
   }
 }
 
-tasks.withType<Detekt>().configureEach {
-  jvmTarget = "14"
+plugins {
+  base
+  kotlin("jvm") apply false
+  id("io.gitlab.arturbosch.detekt")
 }
 
-detekt {
-  config = files("detekt.yml")
-  baseline = file("detekt-baseline.xml")
+allprojects {
+  group = "net.olegg.aoc"
+  version = "2022.0.0"
+
+  repositories {
+    mavenCentral()
+  }
+
+  apply(plugin = "org.jetbrains.kotlin.jvm")
+  apply(plugin = "io.gitlab.arturbosch.detekt")
+
+  dependencies {
+    val implementation by configurations
+    implementation(KotlinX.coroutines.core)
+  }
+
+  tasks.withType<KotlinCompile> {
+    kotlinOptions {
+      jvmTarget = "14"
+      languageVersion = "1.8"
+      allWarningsAsErrors = true
+      freeCompilerArgs += listOf(
+        "-Xsuppress-version-warnings",
+        "-opt-in=kotlin.RequiresOptIn",
+        "-opt-in=kotlin.ExperimentalStdlibApi",
+        "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+        "-opt-in=kotlinx.coroutines.FlowPreview",
+      )
+    }
+  }
+
+  tasks.withType<Detekt>().configureEach {
+    jvmTarget = "14"
+  }
+
+  detekt {
+    config = rootProject.files("detekt.yml")
+    baseline = rootProject.file("detekt-baseline.xml")
+  }
 }
 
 dependencies {
-  implementation(Kotlin.stdlib.jdk8)
-  implementation("org.jetbrains.kotlin:kotlin-reflect:_")
-  implementation(KotlinX.coroutines.core)
-  implementation(KotlinX.serialization.json)
-
-  implementation(Ktor.client.cio)
-
   detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:_")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,19 @@ pluginManagement {
 }
 
 plugins {
-  id("de.fayard.refreshVersions") version "0.50.2"
-////                          # available:"0.51.0"
+  id("de.fayard.refreshVersions") version "0.51.0"
 }
 
 rootProject.name = "advent-of-code"
+
+include(
+  ":core",
+  ":year2015",
+  ":year2016",
+  ":year2017",
+  ":year2018",
+  ":year2019",
+  ":year2020",
+  ":year2021",
+  ":year2022",
+)
