b760219458b8397a7c72e5234dfb24e9fe7c726e
Oleg Godovykh <0legg@users.noreply.github.com>
2022-12-09 21:44:38 -0800

    solve 10th day 2nd task

--- src/main/kotlin/net/olegg/aoc/year2022/day10/Day10.kt
+++ src/main/kotlin/net/olegg/aoc/year2022/day10/Day10.kt
@@ -2,6 +2,7 @@ package net.olegg.aoc.year2022.day10
 
 import net.olegg.aoc.someday.SomeDay
 import net.olegg.aoc.year2022.DayOf2022
+import kotlin.math.abs
 
 /**
  * See [Year 2022, Day 10](https://adventofcode.com/2022/day/10)
@@ -13,38 +14,54 @@ object Day10 : DayOf2022(10) {
   override fun first(): Any? {
     val ops = lines.map { Op.parse(it) }
 
-    val values = buildList {
-      var x = 1L
-      add(x)
-      ops.forEach {
-        repeat(it.cycles) {
-          add(x)
-        }
-        x = it.apply(x)
+    val values = ops
+      .runningFold(1 to emptyList<Int>()) { (last, _), op ->
+        op.apply(last) to List(op.cycles) { last }
+      }
+      .flatMap { it.second }
+
+    return times.sumOf { time -> time * values[time - 1] }
+  }
+
+  override fun second(): Any? {
+    val ops = lines.map { Op.parse(it) }
+    val values = ops
+      .runningFold(1 to emptyList<Int>()) { (last, _), op ->
+        op.apply(last) to List(op.cycles) { last }
+      }
+      .flatMap { it.second }
+
+    val lit = values.chunked(40) { row ->
+      row.mapIndexed { index, position ->
+        abs(index - position) < 2
       }
     }
 
-    return times.sumOf { time -> time * values[time] }
+    return lit.joinToString(separator = "\n", prefix = "\n") {
+      row -> row.joinToString(separator = "") {
+        if (it) "██" else ".."
+      }
+    }
   }
 
   sealed class Op(
     val cycles: Int,
   ) {
-    open fun apply(x: Long): Long = x
+    open fun apply(x: Int): Int = x
 
     object Noop : Op(1)
 
     data class AddX(
-      val value: Long,
+      val value: Int,
     ) : Op(2) {
-      override fun apply(x: Long): Long = x + value
+      override fun apply(x: Int): Int = x + value
     }
 
     companion object {
       fun parse(raw: String): Op {
         return when {
           raw == "noop" -> Noop
-          raw.startsWith("addx ") -> AddX(raw.split(" ").last().toLong())
+          raw.startsWith("addx ") -> AddX(raw.split(" ").last().toInt())
           else -> error("Unable to parse $raw")
         }
       }
