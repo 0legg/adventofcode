57040c6fbd4582aa3ec077a340685b577176e303
Oleg Godovykh <0legg@users.noreply.github.com>
2022-12-11 21:22:58 -0800

    solve 12th day 2nd task

--- year2022/src/main/kotlin/net/olegg/aoc/year2022/day12/Day12.kt
+++ year2022/src/main/kotlin/net/olegg/aoc/year2022/day12/Day12.kt
@@ -7,7 +7,6 @@ import net.olegg.aoc.utils.fit
 import net.olegg.aoc.utils.get
 import net.olegg.aoc.utils.set
 import net.olegg.aoc.year2022.DayOf2022
-import kotlin.math.abs
 
 /**
  * See [Year 2022, Day 12](https://adventofcode.com/2022/day/12)
@@ -46,6 +45,45 @@ object Day12 : DayOf2022(12) {
         .filter { map.fit(it) }
         .filter { it !in visited }
         .filter { map[it]!!- map[curr]!! <= 1 }
+        .map { it to step + 1 }
+    }
+
+    return null
+  }
+
+  override fun second(): Any? {
+    val map = matrix.map { it.toMutableList() }
+    val from = map.mapIndexedNotNull { y, row ->
+      row.mapIndexedNotNull { x, char ->
+        if (char == 'S') Vector2D(x, y) else null
+      }.firstOrNull()
+    }.first()
+    map[from] = 'a'
+
+    val to = map.mapIndexedNotNull { y, row ->
+      row.mapIndexedNotNull { x, char ->
+        if (char == 'E') Vector2D(x, y) else null
+      }.firstOrNull()
+    }.first()
+    map[to] = 'z'
+
+    val visited = mutableSetOf<Vector2D>()
+    val queue = ArrayDeque(listOf(to to 0))
+
+    while (queue.isNotEmpty()) {
+      val (curr, step) = queue.removeFirst()
+      if (map[curr] == 'a') {
+        return step
+      }
+      if (curr in visited) {
+        continue
+      }
+      visited += curr
+
+      queue += Neighbors4.map { curr + it.step }
+        .filter { map.fit(it) }
+        .filter { it !in visited }
+        .filter { map[it]!!- map[curr]!! >= -1 }
         .map { it to step + 1 }
     }
 
