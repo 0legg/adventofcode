aacd16ee896ff326cd2e16c45b68158fcf181b9f
Oleg Godovykh <0legg@users.noreply.github.com>
2022-12-16 22:03:44 -0800

    solve 17th day 2nd task

--- year2022/src/main/kotlin/net/olegg/aoc/year2022/day17/Day17.kt
+++ year2022/src/main/kotlin/net/olegg/aoc/year2022/day17/Day17.kt
@@ -3,6 +3,8 @@ package net.olegg.aoc.year2022.day17
 import net.olegg.aoc.someday.SomeDay
 import net.olegg.aoc.utils.Vector2D
 import net.olegg.aoc.utils.fit
+import net.olegg.aoc.utils.gcd
+import net.olegg.aoc.utils.lcf
 import net.olegg.aoc.year2022.DayOf2022
 
 /**
@@ -36,6 +38,14 @@ object Day17 : DayOf2022(17) {
   ).map { raw -> raw.lines().map { it.toList() } }
 
   override fun first(): Any? {
+    return solve(2022L)
+  }
+
+  override fun second(): Any? {
+    return solve(1000000000000L)
+  }
+
+  private fun solve(steps: Long): Long {
     val jets = data
       .map { char ->
         when (char) {
@@ -47,9 +57,15 @@ object Day17 : DayOf2022(17) {
 
     val stack = ArrayDeque<MutableList<Char>>()
     var jetIndex = 0
+    val heights = mutableListOf<Int>()
 
-    repeat(2022) { step ->
-      val rock = rocks[step % rocks.size]
+    val seen = mutableMapOf<Triple<Int, Int, Vector2D>, Int>()
+
+    val until = steps.coerceAtMost(Int.MAX_VALUE.toLong()).toInt()
+
+    repeat(until) { step ->
+      val rockIndex = step % rocks.size
+      val rock = rocks[rockIndex]
       repeat(3 + rock.size) {
         stack.addFirst(MutableList(7) { '.' })
       }
@@ -57,12 +73,12 @@ object Day17 : DayOf2022(17) {
       var counter = 0
       while (true) {
         if (counter % 2 == 0) {
-          val jet = jets[jetIndex % jets.size]
+          val jet = jets[jetIndex]
           val next = Vector2D(position.x + jet, position.y)
           if (canMove(rock, stack, next)) {
             position = next
           }
-          jetIndex++
+          jetIndex = (jetIndex + 1) % jets.size
         } else {
           val next = Vector2D(position.x, position.y + 1)
           if (canMove(rock, stack, next)) {
@@ -85,9 +101,26 @@ object Day17 : DayOf2022(17) {
       while (stack.first().all { it == '.' }) {
         stack.removeFirst()
       }
+
+      val state = Triple(rockIndex, jetIndex, position)
+      if (state in seen) {
+        val prev = seen.getValue(state)
+        val loopSize = step - prev
+        val loops = (steps - prev) / loopSize
+        val tailSize = steps - prev - loopSize * loops
+
+        val head = heights[prev].toLong()
+        val body = (stack.size.toLong() - head) * loops
+        val tail = heights[prev + tailSize.toInt() - 1] - head
+        return head + body + tail
+      } else {
+        seen[state] = step
+      }
+
+      heights.add(stack.size)
     }
 
-    return stack.size
+    return stack.size.toLong()
   }
 
   private fun canMove(rock: List<List<Char>>, stack: List<List<Char>>, topLeft: Vector2D): Boolean {
