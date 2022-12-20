4551625c071a19e843ba7e6abd3be8847189517a
Oleg Godovykh <0legg@users.noreply.github.com>
2022-12-19 20:31:18 -0800

    solve 19th day 2nd task

--- year2022/src/main/kotlin/net/olegg/aoc/year2022/day19/Day19.kt
+++ year2022/src/main/kotlin/net/olegg/aoc/year2022/day19/Day19.kt
@@ -13,130 +13,137 @@ object Day19 : DayOf2022(19) {
     "Each clay robot costs (\\d+) ore\\. " +
     "Each obsidian robot costs (\\d+) ore and (\\d+) clay\\. " +
     "Each geode robot costs (\\d+) ore and (\\d+) obsidian\\.").toRegex()
-  override fun first(): Any? {
-    val blueprints = lines
-      .mapNotNull { template.find(it) }
-      .map {
-        val (number, oreOre, clayOre, obsidianOre, obsidianClay, geodeOre, geodeObsidian) = it.destructured
 
-        Blueprint(
-          number = number.toInt(),
-          configs = listOf(
-            Robot(
-              cost = Rocks(
-                ore = oreOre.toInt(),
-              ).vector,
-              produce = Rocks(
-                ore = 1,
-              ).vector,
-            ),
-            Robot(
-              cost = Rocks(
-                ore = clayOre.toInt(),
-              ).vector,
-              produce = Rocks(
-                clay = 1,
-              ).vector,
-            ),
-            Robot(
-              cost = Rocks(
-                ore = obsidianOre.toInt(),
-                clay = obsidianClay.toInt(),
-              ).vector,
-              produce = Rocks(
-                obsidian = 1,
-              ).vector,
-            ),
-            Robot(
-              cost = Rocks(
-                ore = geodeOre.toInt(),
-                obsidian = geodeObsidian.toInt(),
-              ).vector,
-              produce = Rocks(
-                geode = 1,
-              ).vector,
-            ),
+  private val blueprints = lines
+    .mapNotNull { template.find(it) }
+    .map {
+      val (number, oreOre, clayOre, obsidianOre, obsidianClay, geodeOre, geodeObsidian) = it.destructured
+
+      Blueprint(
+        number = number.toInt(),
+        configs = listOf(
+          Robot(
+            cost = Rocks(
+              ore = oreOre.toInt(),
+            ).vector,
+            produce = Rocks(
+              ore = 1,
+            ).vector,
           ),
-        )
-      }
-
-    return blueprints.sumOf { blueprint ->
-      val initialState = State(
-        robots = Vector4D(1, 0, 0, 0),
-        resources = Vector4D(0, 0, 0, 0),
-        time = 24,
+          Robot(
+            cost = Rocks(
+              ore = clayOre.toInt(),
+            ).vector,
+            produce = Rocks(
+              clay = 1,
+            ).vector,
+          ),
+          Robot(
+            cost = Rocks(
+              ore = obsidianOre.toInt(),
+              clay = obsidianClay.toInt(),
+            ).vector,
+            produce = Rocks(
+              obsidian = 1,
+            ).vector,
+          ),
+          Robot(
+            cost = Rocks(
+              ore = geodeOre.toInt(),
+              obsidian = geodeObsidian.toInt(),
+            ).vector,
+            produce = Rocks(
+              geode = 1,
+            ).vector,
+          ),
+        ),
       )
+    }
 
-      val queue = PriorityQueue<State>(
-        compareBy(
-          { -it.robots.manhattan() },
-          { -it.resources.w },
-          { it.time },
-        )
-      ).apply {
-        this += initialState
+  override fun first(): Any? {
+    return blueprints.take(1).sumOf { blueprint ->
+      maxGeodes(blueprint, 24) * blueprint.number
+    }
+  }
+
+  override fun second(): Any? {
+    return blueprints.take(3)
+      .map { maxGeodes(it, 32) }
+      .reduce(Int::times)
+  }
+
+  private fun maxGeodes(blueprint: Blueprint, time: Int): Int {
+    val initialState = State(
+      robots = Vector4D(1, 0, 0, 0),
+      resources = Vector4D(0, 0, 0, 0),
+      time = time,
+    )
+
+    val queue = PriorityQueue<State>(
+      compareBy(
+        { -it.robots.manhattan() },
+        { -it.resources.w },
+        { it.time },
+      )
+    ).apply {
+      this += initialState
+    }
+    val seen = mutableSetOf<State>()
+    var best = 0
+
+    while (queue.isNotEmpty()) {
+      val curr = queue.remove()
+      if (curr in seen) {
+        continue
       }
-      val seen = mutableSetOf<State>()
-      var best = 0
-      var iter = 0L
+      seen += curr
 
-      while (queue.isNotEmpty()) {
-        val curr = queue.remove()
-        if (iter++ % 1000000L == 0L) {
-          println("${queue.size}, ${curr.time}, $best")
-        }
-        if (curr in seen) {
-          continue
-        }
-        seen += curr
+      val newResources = curr.resources + curr.robots
+      if (curr.time == 1) {
+        best = maxOf(best, newResources.w)
+      } else {
+        val newState = State(
+          robots = curr.robots,
+          resources = newResources,
+          time = curr.time - 1,
+        )
 
-        val newResources = curr.resources + curr.robots
-        if (curr.time == 1) {
-          best = maxOf(best, newResources.w)
-        } else {
-          val newState = State(
-            robots = curr.robots,
-            resources = newResources,
-            time = curr.time - 1,
-          )
-
-          if (newState !in seen) {
-            val possibleBest = newState.resources.w +
-              newState.robots.w * newState.time +
-              newState.time * (newState.time - 1) / 2
-            if (possibleBest > best) {
-              queue.add(newState)
-            }
+        if (newState !in seen) {
+          val possibleBest = newState.resources.w +
+            newState.robots.w * newState.time +
+            newState.time * (newState.time - 1) / 2
+          if (possibleBest > best) {
+            queue.add(newState)
           }
         }
+      }
 
-        blueprint.configs.forEach { config ->
-          val buildCost = config.cost
-          val afterBuild = curr.resources - buildCost
-          if (afterBuild.toList().all { it >= 0 }) {
-            if (curr.time == 1) {
-              best = maxOf(best, afterBuild.w)
-            } else {
-              val newState = State(
-                robots = curr.robots + config.produce,
-                resources = afterBuild + curr.robots,
-                time = curr.time - 1,
-              )
-              if (newState !in seen) {
-                val possibleBest = newState.resources.w +
-                  newState.robots.w * newState.time +
-                  newState.time * (newState.time - 1) / 2
-                if (possibleBest > best) {
-                  queue.add(newState)
-                }
+      blueprint.configs.forEach { config ->
+        val buildCost = config.cost
+        val afterBuild = curr.resources - buildCost
+        if (afterBuild.toList().all { it >= 0 }) {
+          if (curr.time == 1) {
+            best = maxOf(best, afterBuild.w)
+          } else {
+            val newState = State(
+              robots = curr.robots + config.produce,
+              resources = afterBuild + curr.robots,
+              time = curr.time - 1,
+            )
+            if (newState !in seen) {
+              val possibleBest = newState.resources.w +
+                newState.robots.w * newState.time +
+                newState.time * (newState.time - 1) / 2
+              if (possibleBest > best) {
+                queue.add(newState)
               }
             }
           }
         }
       }
-
-      best * blueprint.number
     }
+
+    return best
   }
 
   data class Rocks(
