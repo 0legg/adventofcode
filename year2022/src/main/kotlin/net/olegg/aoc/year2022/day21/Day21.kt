5b274cba6551aa423ecd383c5f704150ebde3a3e
Oleg Godovykh <0legg@users.noreply.github.com>
2022-12-20 22:14:28 -0800

    solve 21st day 2nd task

--- year2022/src/main/kotlin/net/olegg/aoc/year2022/day21/Day21.kt
+++ year2022/src/main/kotlin/net/olegg/aoc/year2022/day21/Day21.kt
@@ -7,6 +7,8 @@ import net.olegg.aoc.year2022.DayOf2022
  * See [Year 2022, Day 21](https://adventofcode.com/2022/day/21)
  */
 object Day21 : DayOf2022(21) {
+  private val root = "(root): ([a-z]+) ([-+/*]) ([a-z]+)".toRegex()
+  private val human = "(humn): ([0-9]+)".toRegex()
   private val op = "([a-z]+): ([a-z]+) ([-+/*]) ([a-z]+)".toRegex()
   private val number = "([a-z]+): ([0-9]+)".toRegex()
 
@@ -79,10 +81,138 @@ object Day21 : DayOf2022(21) {
     return (monkeys["root"] as Monkey.Value).value
   }
 
+  override fun second(): Any? {
+    val monkeys = lines.mapNotNull { line ->
+      when {
+        root.matches(line) -> root.find(line)?.let { match ->
+          val (name, left, _, right) = match.destructured
+          Monkey.Root(
+            name = name,
+            left = left,
+            right = right,
+          )
+        }
+        human.matches(line) -> human.find(line)?.let { match ->
+          val (name, _) = match.destructured
+          Monkey.Human(
+            name = name,
+            value = 0L,
+          )
+        }
+        op.matches(line) -> op.find(line)?.let { match ->
+          val (name, left, op, right) = match.destructured
+          when (op) {
+            "+" -> Monkey.Op.Plus(
+              name = name,
+              left = left,
+              right = right,
+            )
+            "-" -> Monkey.Op.Minus(
+              name = name,
+              left = left,
+              right = right,
+            )
+            "*" -> Monkey.Op.Mult(
+              name = name,
+              left = left,
+              right = right,
+            )
+            "/" -> Monkey.Op.Div(
+              name = name,
+              left = left,
+              right = right,
+            )
+            else -> null
+          }
+        }
+        number.matches(line) -> number.find(line)?.let { match ->
+          val (name, number) = match.destructured
+          Monkey.Value(
+            name = name,
+            value = number.toLong(),
+          )
+        }
+        else -> null
+      }
+    }
+      .associateBy { it.name }
+      .toMutableMap()
+
+    do {
+      val canResolve = monkeys
+        .filter { it.value is Monkey.Op }
+        .filter {
+          val op = it.value as Monkey.Op
+          monkeys[op.left] is Monkey.Value && monkeys[op.right] is Monkey.Value
+        }
+
+      canResolve.forEach { (key, value) ->
+        val op = value as Monkey.Op
+        val left = monkeys[op.left] as Monkey.Value
+        val right = monkeys[op.right] as Monkey.Value
+        monkeys[key] = Monkey.Value(
+          name = key,
+          value = when (op) {
+            is Monkey.Op.Plus -> left.value + right.value
+            is Monkey.Op.Minus -> left.value - right.value
+            is Monkey.Op.Mult -> left.value * right.value
+            is Monkey.Op.Div -> left.value / right.value
+          }
+        )
+      }
+    } while (canResolve.isNotEmpty())
+
+    val root = monkeys["root"] as Monkey.Root
+    val rootLeft = monkeys[root.left]
+    val rootRight = monkeys[root.right]
+
+    val (rootOp, rootValue) = when {
+      rootLeft is Monkey.Op && rootRight is Monkey.Value -> rootLeft to rootRight
+      rootRight is Monkey.Op && rootLeft is Monkey.Value -> rootRight to rootLeft
+      else -> error("Can't resolve root")
+    }
+
+    var curr: Monkey = rootOp
+    var currValue = rootValue.value
+    while (curr is Monkey.Op) {
+      val left = monkeys[curr.left]
+      val right = monkeys[curr.right]
+      val (nextOp, sideValue) = when {
+        left is Monkey.Op && right is Monkey.Value -> left to right.value
+        right is Monkey.Op && left is Monkey.Value -> right to left.value
+        left is Monkey.Human && right is Monkey.Value -> left to right.value
+        right is Monkey.Human && left is Monkey.Value -> right to left.value
+        else -> error("Can't resolve $curr $left $right")
+      }
+
+      val nextValue = when (curr) {
+        is Monkey.Op.Plus -> currValue - sideValue
+        is Monkey.Op.Minus -> if (left is Monkey.Value) sideValue - currValue else sideValue + currValue
+        is Monkey.Op.Mult -> currValue / sideValue
+        is Monkey.Op.Div -> if (left is Monkey.Value) sideValue / currValue else sideValue * currValue
+      }
+      curr = nextOp
+      currValue = nextValue
+    }
+
+    return currValue
+  }
+
   sealed interface Monkey {
     val name: String
 
     data class Value(
+      override val name: String,
+      val value: Long,
+    ) : Monkey
+
+    data class Root(
+      override val name: String,
+      val left: String,
+      val right: String,
+    ) : Monkey
+
+    data class Human(
       override val name: String,
       val value: Long,
     ) : Monkey
