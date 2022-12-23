7688f947e2c8e9b2831f5cb2dfd4358dd34a93b1
Oleg Godovykh <0legg@users.noreply.github.com>
2022-12-22 21:28:55 -0800

    solve 23th day 2nd task

--- year2022/src/main/kotlin/net/olegg/aoc/year2022/day23/Day23.kt
+++ year2022/src/main/kotlin/net/olegg/aoc/year2022/day23/Day23.kt
@@ -60,6 +60,51 @@ object Day23 : DayOf2022(23) {
 
     return (maxX - minX + 1) * (maxY - minY + 1) - finish.size
   }
+
+  override fun second(): Any? {
+    var elves = matrix
+      .flatMapIndexed { y, row ->
+        row.mapIndexedNotNull { x, c ->
+          Vector2D(x, y).takeIf { c == '#' }
+        }
+      }
+
+    val dirs = ArrayDeque(
+      listOf(
+        U to listOf(UL, U, UR),
+        D to listOf(DL, D, DR),
+        L to listOf(UL, L, DL),
+        R to listOf(UR, R, DR),
+      )
+    )
+
+    var moves = 0
+    do {
+      moves++
+      val proposed = elves.map { elf ->
+        if (Directions.Neighbors8.none { elf + it.step in elves }) {
+          elf
+        } else {
+          val maybeDir = dirs.firstOrNull { dir ->
+            dir.second.none { elf + it.step in elves }
+          }
+          maybeDir?.let { elf + it.first.step } ?: elf
+        }
+      }
+
+      val counts = proposed.groupingBy { it }.eachCount()
+
+      val nextElves = elves.zip(proposed) { curr, next ->
+        if (counts[next] == 1) next else curr
+      }
+
+      val moved = elves.zip(nextElves).any { it.first != it.second }
+      elves = nextElves
+      dirs.addLast(dirs.removeFirst())
+    } while (moved)
+
+    return moves
+  }
 }
 
 fun main() = SomeDay.mainify(Day23)
