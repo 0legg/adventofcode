2f50fb2e7386c69cdbd785fc428e882ee647402d
Oleg Godovykh <0legg@users.noreply.github.com>
2023-11-30 21:27:36 -0800

    solve 1st day 2nd task

--- core/src/main/kotlin/net/olegg/aoc/utils/Strings.kt
+++ core/src/main/kotlin/net/olegg/aoc/utils/Strings.kt
@@ -20,3 +20,18 @@ fun String?.parseLongs(
   vararg delimiters: String,
   radix: Int = 10
 ): List<Long> = this?.split(*delimiters)?.mapNotNull { it.toLongOrNull(radix) }.orEmpty()
+
+/**
+ * Finds all occurrences of [this] in [input] starting from [startIndex], including overlapping ones.
+ * So, while `"aa".toRegex().findAll("aaa").count()` returns 1,
+ * `"aa".toRegex().findAllOverlapping("aaa").count()` will return 2, both for first two and last two characters.
+ */
+fun Regex.findAllOverlapping(
+  input: CharSequence,
+  startIndex: Int = 0,
+): Sequence<MatchResult> {
+  require(startIndex in input.indices)
+  return generateSequence({ find(input, startIndex) }) { previous ->
+    find(input, previous.range.first + 1)
+  }
+}
