42acda47e1243e5c8b53b68fa980e5f21539e5eb
Oleg Godovykh <0legg@users.noreply.github.com>
2023-12-07 21:30:36 -0800

    solve 8th day 2nd task

--- year2023/src/main/kotlin/net/olegg/aoc/year2023/day8/Day8.kt
+++ year2023/src/main/kotlin/net/olegg/aoc/year2023/day8/Day8.kt
@@ -8,7 +8,6 @@ import net.olegg.aoc.year2023.DayOf2023
  */
 object Day8 : DayOf2023(8) {
   private val PATTERN = "(\\w+) = \\((\\w+), (\\w+)\\)".toRegex()
-  private val DIRS = "LR"
 
   override fun first(): Any? {
     val (instruction, nodeStrings) = data.split("\n\n")
@@ -16,17 +15,41 @@ object Day8 : DayOf2023(8) {
     val nodes = nodeStrings.lineSequence()
       .mapNotNull { PATTERN.find(it) }
       .associate {
-        it.groupValues[1] to (it.groupValues[2] to it.groupValues[3])
+        it.groupValues[1] to mapOf('L' to it.groupValues[2], 'R' to it.groupValues[3])
       }
 
     return generateSequence(0) { it + 1 }
       .runningFold("AAA" to 0) { (curr, steps), index ->
         val char = instruction[index % instruction.length]
-        val direction = DIRS.indexOf(char)
-        nodes.getValue(curr).toList()[direction] to steps + 1
+        nodes.getValue(curr).getValue(char) to steps + 1
       }
       .first { it.first == "ZZZ" }
       .second
+  }
+
+  override fun second(): Any? {
+    val (instruction, nodeStrings) = data.split("\n\n")
+
+    val nodes = nodeStrings.lineSequence()
+      .mapNotNull { PATTERN.find(it) }
+      .associate {
+        it.groupValues[1] to mapOf('L' to it.groupValues[2], 'R' to it.groupValues[3])
+      }
+
+    val aNodes = nodes.keys.filter { it.endsWith("A") }
+
+    val loops = aNodes.map { node ->
+      generateSequence(0L) { it + 1 }
+        .runningFold(node to 0L) { (curr, steps), index ->
+          val char = instruction[(index % instruction.length).toInt()]
+          nodes.getValue(curr).getValue(char) to steps + 1
+        }
+        .first { it.first.endsWith("Z") }
+        .second
+    }
+
+    return loops.map { it / instruction.length }
+      .fold(instruction.length.toLong(), Long::times)
   }
 }
 
