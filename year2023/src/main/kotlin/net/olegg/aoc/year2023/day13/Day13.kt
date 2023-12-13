f61bfc774599c975d2c0d5a751956e2f86ae017e
Oleg Godovykh <0legg@users.noreply.github.com>
2023-12-12 21:23:38 -0800

    solve 13th day 2nd task

--- year2023/src/main/kotlin/net/olegg/aoc/year2023/day13/Day13.kt
+++ year2023/src/main/kotlin/net/olegg/aoc/year2023/day13/Day13.kt
@@ -8,25 +8,45 @@ import net.olegg.aoc.year2023.DayOf2023
  */
 object Day13 : DayOf2023(13) {
   override fun first(): Any? {
-    val patterns = data.split("\n\n").map { it.lines() }
+    val patterns = data.split("\n\n").map { pattern -> pattern.lines().map { it.toList() } }
     val transposed = patterns.map { it.transpose() }
 
     return transposed.sumOf { it.rowsBeforeReflection() } + 100 * patterns.sumOf { it.rowsBeforeReflection() }
   }
 
-  private fun List<String>.transpose(): List<String> {
-    return List(first().length) { index ->
-      String(map { it[index] }.toCharArray())
+  override fun second(): Any? {
+    val patterns = data.split("\n\n").map { pattern -> pattern.lines().map { it.toList() } }
+    val transposed = patterns.map { it.transpose() }
+
+    return transposed.sumOf { it.rowsBeforeSmudge() } + 100 * patterns.sumOf { it.rowsBeforeSmudge() }
+  }
+
+  private fun List<List<Char>>.transpose(): List<List<Char>> {
+    return List(first().size) { row ->
+      List(size) { column -> this[column][row] }
     }
   }
 
-  private fun List<String>.rowsBeforeReflection(): Int {
+  private fun List<List<Char>>.rowsBeforeReflection(): Int {
     val rev = asReversed()
     for (i in 1..<size) {
       val below = drop(i)
       val above = rev.takeLast(i)
 
-      if (below.zip(above).all { (a, b) -> a == b }) {
+      if (above.zip(below).all { (a, b) -> a == b }) {
+        return i
+      }
+    }
+    return 0
+  }
+
+  private fun List<List<Char>>.rowsBeforeSmudge(): Int {
+    val rev = asReversed()
+    for (i in 1..<size) {
+      val below = drop(i)
+      val above = rev.takeLast(i)
+
+      if (above.zip(below).sumOf { (a, b) -> a.zip(b).count { (ca, cb) -> ca != cb } } == 1) {
         return i
       }
     }
