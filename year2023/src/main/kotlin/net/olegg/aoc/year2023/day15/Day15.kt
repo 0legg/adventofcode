7a2ca48ea277040dbcb2f2d19e855107eca08957
Oleg Godovykh <0legg@users.noreply.github.com>
2023-12-14 21:20:06 -0800

    solve 15th day 2nd task

--- year2023/src/main/kotlin/net/olegg/aoc/year2023/day15/Day15.kt
+++ year2023/src/main/kotlin/net/olegg/aoc/year2023/day15/Day15.kt
@@ -1,7 +1,9 @@
 package net.olegg.aoc.year2023.day15
 
 import net.olegg.aoc.someday.SomeDay
+import net.olegg.aoc.utils.toPair
 import net.olegg.aoc.year2023.DayOf2023
+import net.olegg.aoc.year2023.day15.Day15.Step.Companion.toStep
 
 /**
  * See [Year 2023, Day 15](https://adventofcode.com/2023/day/15)
@@ -9,9 +11,47 @@ import net.olegg.aoc.year2023.DayOf2023
 object Day15 : DayOf2023(15) {
   override fun first(): Any? {
     return data.split(",").sumOf { step ->
-      step.fold(0) { acc, char ->
-        (acc + char.code) * 17 % 256
-      }.toInt()
+      step.hash()
+    }
+  }
+
+  override fun second(): Any? {
+    val steps = data.split(",").map { it.toStep() }
+
+    val boxes = List(256) { LinkedHashMap<String, Int>() }
+
+    steps.forEach { step ->
+      val index = step.label.hash()
+      when (step) {
+        is Step.Remove -> boxes[index].remove(step.label)
+        is Step.Add -> boxes[index][step.label] = step.value
+      }
+    }
+
+    return boxes.mapIndexed { box, lenses ->
+      (box + 1) * lenses.values.mapIndexed { index, value -> (index + 1) * value }.sum()
+    }.sum()
+  }
+
+  private fun String.hash() = fold(0) { acc, char -> (acc + char.code) * 17 % 256 }
+
+  sealed interface Step {
+    val label: String
+
+    data class Add(
+      override val label: String,
+      val value: Int,
+    ) : Step
+
+    data class Remove(
+      override val label: String,
+    ) : Step
+
+    companion object {
+      fun String.toStep() = when {
+        last() == '-' -> Remove(dropLast(1))
+        else -> split("=").toPair().let { Add(it.first, it.second.toInt()) }
+      }
     }
   }
 }
