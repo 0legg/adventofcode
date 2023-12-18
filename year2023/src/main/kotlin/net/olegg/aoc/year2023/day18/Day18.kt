d5b35a661cdbfd21773935174076863233580c79
Oleg Godovykh <0legg@users.noreply.github.com>
2023-12-17 23:13:26 -0800

    solve 18th day 2nd task

--- year2023/src/main/kotlin/net/olegg/aoc/year2023/day18/Day18.kt
+++ year2023/src/main/kotlin/net/olegg/aoc/year2023/day18/Day18.kt
@@ -2,9 +2,11 @@ package net.olegg.aoc.year2023.day18
 
 import net.olegg.aoc.someday.SomeDay
 import net.olegg.aoc.utils.Directions
+import net.olegg.aoc.utils.Directions.D
+import net.olegg.aoc.utils.Directions.L
+import net.olegg.aoc.utils.Directions.R
+import net.olegg.aoc.utils.Directions.U
 import net.olegg.aoc.utils.Vector2D
-import net.olegg.aoc.utils.get
-import net.olegg.aoc.utils.set
 import net.olegg.aoc.utils.toPair
 import net.olegg.aoc.year2023.DayOf2023
 
@@ -16,43 +18,33 @@ object Day18 : DayOf2023(18) {
     val steps = lines.map { it.split(" ").toPair() }
       .map { Directions.valueOf(it.first) to it.second.toInt() }
 
-    val map = mutableSetOf(Vector2D())
-    steps.fold(Vector2D()) { acc, (step, count) ->
-      (1..count).scan(acc) { smallAcc, _ ->
-        smallAcc + step.step
-      }.onEach { map += it }.last()
-    }
-
-    val minX = map.minOf { it.x } - 1
-    val maxX = map.maxOf { it.x } + 1
-    val minY = map.minOf { it.y } - 1
-    val maxY = map.maxOf { it.y } + 1
-
-    val shift = Vector2D(minX, minY)
-
-    val fill = List(maxY - minY + 1) { y ->
-      MutableList(maxX - minX + 1) { x ->
-        if (Vector2D(x, y) + shift in map) '#' else ' '
-      }
-    }
-
-    val queue = ArrayDeque(listOf(Vector2D()))
-    while (queue.isNotEmpty()) {
-      val curr = queue.removeFirst()
-      if (fill[curr] != ' ') {
-        continue
-      }
-      fill[curr] = '.'
-
-      queue += Directions.NEXT_4
-        .map { curr + it.step }
-        .filter { fill[it] == ' ' }
-    }
-
-    return fill.sumOf { row ->
-      row.count { it != '.' }
-    }
+    return solve(steps)
   }
-}
+
+  override fun second(): Any? {
+    val dirMapping = mapOf(
+      '0' to R,
+      '1' to D,
+      '2' to L,
+      '3' to U,
+    )
+    val steps = lines.map { it.takeLast(7).dropLast(1) }
+      .map { dirMapping.getValue(it.last()) to it.take(5).toInt(16) }
+
+    return solve(steps)
+  }
+
+  private fun solve(steps: List<Pair<Directions, Int>>): Long {
+    val points = steps.scan(Vector2D()) { acc, (dir, count) ->
+      acc + dir.step * count
+    }
+    val perimeter = steps.sumOf { it.second.toLong() }
+
+    val xs = points.map { it.x.toLong() }
+    val ys = points.map { it.y.toLong() }
+
+    return (xs.zip(ys.drop(1), Long::times).sum() - ys.zip(xs.drop(1), Long::times).sum()) / 2 + (perimeter / 2 + 1)
+  }
+ }
 
 fun main() = SomeDay.mainify(Day18)
