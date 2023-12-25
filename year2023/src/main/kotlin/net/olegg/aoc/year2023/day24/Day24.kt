0d66af2da164a01523de9aff45d2404c1383fb8e
Oleg Godovykh <0legg@users.noreply.github.com>
2023-12-25 05:55:14 +0100

    solve 24th day 2nd task

--- year2023/src/main/kotlin/net/olegg/aoc/year2023/day24/Day24.kt
+++ year2023/src/main/kotlin/net/olegg/aoc/year2023/day24/Day24.kt
@@ -1,8 +1,11 @@
 package net.olegg.aoc.year2023.day24
 
+import com.microsoft.z3.Context
+import com.microsoft.z3.Status
 import net.olegg.aoc.someday.SomeDay
 import net.olegg.aoc.utils.pairs
 import net.olegg.aoc.utils.parseBigInts
+import net.olegg.aoc.utils.parseLongs
 import net.olegg.aoc.utils.toPair
 import net.olegg.aoc.year2023.DayOf2023
 import java.math.BigInteger
@@ -48,6 +51,86 @@ object Day24 : DayOf2023(24) {
       .count { (x, y) ->
         x in MIN..MAX && y in MIN..MAX
       }
+  }
+
+  @Suppress("UsePropertyAccessSyntax")
+  override fun second(): Any? {
+    val stones = lines.map { it.split(" @ ").toPair() }
+      .map { it.first.parseLongs(", ") to it.second.parseLongs(", ") }
+
+    Context().use { ctx ->
+      val solver = ctx.mkSolver()
+
+      val x0 = ctx.mkConst(
+        ctx.mkSymbol("x"),
+        ctx.getIntSort(),
+      )
+      val y0 = ctx.mkConst(
+        ctx.mkSymbol("y"),
+        ctx.getIntSort(),
+      )
+      val z0 = ctx.mkConst(
+        ctx.mkSymbol("z"),
+        ctx.getIntSort(),
+      )
+      val dx0 = ctx.mkConst(
+        ctx.mkSymbol("dx"),
+        ctx.getIntSort(),
+      )
+      val dy0 = ctx.mkConst(
+        ctx.mkSymbol("dy"),
+        ctx.getIntSort(),
+      )
+      val dz0 = ctx.mkConst(
+        ctx.mkSymbol("dz"),
+        ctx.getIntSort(),
+      )
+      val zero = ctx.mkInt(0)
+
+      stones.forEachIndexed { i, stone ->
+        val (x, y, z) = stone.first
+        val (dx, dy, dz) = stone.second
+
+        val xi = ctx.mkInt(x)
+        val yi = ctx.mkInt(y)
+        val zi = ctx.mkInt(z)
+        val dxi = ctx.mkInt(dx)
+        val dyi = ctx.mkInt(dy)
+        val dzi = ctx.mkInt(dz)
+
+        val ti = ctx.mkConst(
+          ctx.mkSymbol("t_$i"),
+          ctx.getIntSort(),
+        )
+
+        solver.add(
+          ctx.mkGe(ti, zero),
+          ctx.mkEq(
+            ctx.mkAdd(xi, ctx.mkMul(dxi, ti)),
+            ctx.mkAdd(x0, ctx.mkMul(dx0, ti)),
+          ),
+          ctx.mkEq(
+            ctx.mkAdd(yi, ctx.mkMul(dyi, ti)),
+            ctx.mkAdd(y0, ctx.mkMul(dy0, ti)),
+          ),
+          ctx.mkEq(
+            ctx.mkAdd(zi, ctx.mkMul(dzi, ti)),
+            ctx.mkAdd(z0, ctx.mkMul(dz0, ti)),
+          )
+        )
+      }
+
+      if (solver.check() == Status.SATISFIABLE) {
+        val model = solver.model
+        val xs = model.eval(x0, false)
+        val ys = model.eval(y0, false)
+        val zs = model.eval(z0, false)
+
+        return xs.toString().toLong() + ys.toString().toLong() + zs.toString().toLong()
+      }
+    }
+
+    return -1
   }
 }
 
