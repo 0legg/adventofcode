4e49376e273d5b6653b33dd4954afc71318f98ef
Oleg Godovykh <0legg@users.noreply.github.com>
2024-11-30 21:05:25 -0800

    solve 1st day 1st task

---
+++ year2024/src/main/kotlin/net/olegg/aoc/year2024/day1/Day1.kt
@@ -0,0 +1,23 @@
+package net.olegg.aoc.year2024.day1
+
+import net.olegg.aoc.someday.SomeDay
+import net.olegg.aoc.utils.parseInts
+import net.olegg.aoc.utils.transpose
+import net.olegg.aoc.year2024.DayOf2024
+import kotlin.math.absoluteValue
+
+/**
+ * See [Year 2024, Day 1](https://adventofcode.com/2024/day/1)
+ */
+object Day1 : DayOf2024(1) {
+  override fun first(): Any? {
+    val (first, second) = lines
+      .map { it.parseInts(" ") }
+      .transpose()
+      .map { it.sorted() }
+
+    return first.zip(second) { a, b -> (a - b).absoluteValue }.sum()
+  }
+}
+
+fun main() = SomeDay.mainify(Day1)
