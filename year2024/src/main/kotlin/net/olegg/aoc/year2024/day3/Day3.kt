650bab0735063d14b891b2777ab4f8fd3c4cdd24
Oleg Godovykh <0legg@users.noreply.github.com>
2024-12-02 21:18:52 -0800

    solve 3rd day 2nd task

--- year2024/src/main/kotlin/net/olegg/aoc/year2024/day3/Day3.kt
+++ year2024/src/main/kotlin/net/olegg/aoc/year2024/day3/Day3.kt
@@ -8,10 +8,28 @@ import net.olegg.aoc.year2024.DayOf2024
  */
 object Day3 : DayOf2024(3) {
   private val pattern = """mul\((\d+),(\d+)\)""".toRegex()
+  private val dos = """do\(\)""".toRegex()
+  private val donts = """don't\(\)""".toRegex()
+
   override fun first(): Any? {
     return pattern.findAll(data).sumOf { match ->
       val (a, b) = match.destructured
       a.toLong() * b.toLong()
+    }
+  }
+
+  override fun second(): Any? {
+    val mults = buildList {
+      add(0 to 1)
+      dos.findAll(data).forEach { add(it.range.first to 1) }
+      donts.findAll(data).forEach { add(it.range.first to 0) }
+    }.sortedBy { it.first }
+
+    return pattern.findAll(data).sumOf { match ->
+      val start = match.range.first
+      val (a, b) = match.destructured
+
+      a.toLong() * b.toLong() * mults.last { it.first < start }.second
     }
   }
 }
