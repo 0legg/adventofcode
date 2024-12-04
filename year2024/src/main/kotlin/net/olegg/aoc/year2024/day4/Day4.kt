73b3e051a84d92d00018e1f09845fe806c01c3aa
Oleg Godovykh <0legg@users.noreply.github.com>
2024-12-03 21:20:28 -0800

    solve 4th day 2nd task

--- year2024/src/main/kotlin/net/olegg/aoc/year2024/day4/Day4.kt
+++ year2024/src/main/kotlin/net/olegg/aoc/year2024/day4/Day4.kt
@@ -2,8 +2,13 @@ package net.olegg.aoc.year2024.day4
 
 import net.olegg.aoc.someday.SomeDay
 import net.olegg.aoc.utils.Directions
+import net.olegg.aoc.utils.Directions.DL
+import net.olegg.aoc.utils.Directions.DR
+import net.olegg.aoc.utils.Directions.UL
+import net.olegg.aoc.utils.Directions.UR
 import net.olegg.aoc.utils.Vector2D
 import net.olegg.aoc.utils.get
+import net.olegg.aoc.utils.permutations
 import net.olegg.aoc.year2024.DayOf2024
 
 /**
@@ -24,6 +29,31 @@ object Day4 : DayOf2024(4) {
         }
       }
     }.count()
+  }
+
+  override fun second(): Any? {
+    val corners = listOf(
+      listOf(listOf(UL, UR), listOf(DL, DR)),
+      listOf(listOf(UL, DL), listOf(UR, DR)),
+    )
+    val letters = listOf('M', 'S')
+
+    return matrix.flatMapIndexed { y, row ->
+      row.mapIndexed { x, c ->
+        val curr = Vector2D(x, y)
+        when {
+          c != 'A' -> 0
+          corners.none { currCorners ->
+            letters.permutations().any { currLetters ->
+              currCorners.zip(currLetters).all { (corner, letter) ->
+                corner.all { dir -> matrix[curr + dir.step] == letter }
+              }
+            }
+          } -> 0
+          else -> 1
+        }
+      }
+    }.sum()
   }
 }
 
