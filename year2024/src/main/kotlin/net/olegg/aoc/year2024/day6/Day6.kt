951d68490ec1ba723e49d8b3bb992a0bef1aa8a5
Oleg Godovykh <0legg@users.noreply.github.com>
2024-12-05 21:14:08 -0800

    solve 6th day 2nd task

--- year2024/src/main/kotlin/net/olegg/aoc/year2024/day6/Day6.kt
+++ year2024/src/main/kotlin/net/olegg/aoc/year2024/day6/Day6.kt
@@ -1,6 +1,7 @@
 package net.olegg.aoc.year2024.day6
 
 import net.olegg.aoc.someday.SomeDay
+import net.olegg.aoc.utils.Directions
 import net.olegg.aoc.utils.Directions.Companion.CW
 import net.olegg.aoc.utils.Directions.U
 import net.olegg.aoc.utils.Vector2D
@@ -28,6 +29,47 @@ object Day6 : DayOf2024(6) {
     }
 
     return visited.size
+  }
+
+  override fun second(): Any? {
+    val start = matrix.find('^') ?: return null
+
+    var curr = start
+    var dir = U
+    val visited = mutableSetOf<Vector2D>()
+    while (matrix[curr] != null) {
+      visited += curr
+      if (matrix[curr + dir.step] != '#') {
+        curr = curr + dir.step
+      } else {
+        dir = CW.getValue(dir)
+      }
+    }
+
+    visited -= start
+
+    return visited.count { obstacle ->
+      val newMatrix = matrix.mapIndexed { y, row ->
+        row.mapIndexed { x, c ->
+          if (obstacle.x == x && obstacle.y == y) '#' else c
+        }
+      }
+
+      var newCurr = start
+      var newDir = U
+      val seen = mutableSetOf<Pair<Vector2D, Directions>>()
+
+      while (newMatrix[newCurr] != null && (newCurr to newDir) !in seen) {
+        seen += newCurr to newDir
+        if (newMatrix[newCurr + newDir.step] != '#') {
+          newCurr = newCurr + newDir.step
+        } else {
+          newDir = CW.getValue(newDir)
+        }
+      }
+
+      newMatrix[newCurr] != null
+    }
   }
 }
 
