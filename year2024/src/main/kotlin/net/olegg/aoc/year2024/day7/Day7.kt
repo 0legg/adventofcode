c8a9089a74c94839a74e18c29262e16846dbc5ba
Oleg Godovykh <0legg@users.noreply.github.com>
2024-12-06 22:07:39 -0800

    solve 7th day 2nd task

--- year2024/src/main/kotlin/net/olegg/aoc/year2024/day7/Day7.kt
+++ year2024/src/main/kotlin/net/olegg/aoc/year2024/day7/Day7.kt
@@ -3,6 +3,8 @@ package net.olegg.aoc.year2024.day7
 import net.olegg.aoc.someday.SomeDay
 import net.olegg.aoc.utils.parseBigInts
 import net.olegg.aoc.year2024.DayOf2024
+import java.math.BigInteger
+import kotlin.math.pow
 
 /**
  * See [Year 2024, Day 7](https://adventofcode.com/2024/day/7)
@@ -23,6 +25,32 @@ object Day7 : DayOf2024(7) {
           result == terms.drop(1).zip(ops)
             .fold(terms.first()) { acc, (value, op) ->
               if (op == '0') acc + value else acc * value
+            }
+        }
+      }
+      .sumOf { it.first() }
+  }
+
+  override fun second(): Any? {
+    val equations = lines.map { it.parseBigInts(":", " ") }
+
+    return equations
+      .filter { equation ->
+        val result = equation.first()
+        val terms = equation.drop(1)
+        val opsCount = terms.size - 1
+
+        (0..3.0.pow(opsCount.toDouble()).toInt()).any { mask ->
+          val ops = mask.toString(3).padStart(opsCount, '0').toList()
+
+          result == terms.drop(1).zip(ops)
+            .fold(terms.first()) { acc, (value, op) ->
+              when (op) {
+                '0' -> acc + value
+                '1' -> acc * value
+                '2' -> BigInteger("$acc$value")
+                else -> acc
+              }
             }
         }
       }
