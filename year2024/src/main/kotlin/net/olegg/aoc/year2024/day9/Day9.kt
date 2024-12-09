372ddfbdc810dee1c3734f10edef991ba87096c4
Oleg Godovykh <0legg@users.noreply.github.com>
2024-12-08 21:37:35 -0800

    solve 9th day 2nd task

--- year2024/src/main/kotlin/net/olegg/aoc/year2024/day9/Day9.kt
+++ year2024/src/main/kotlin/net/olegg/aoc/year2024/day9/Day9.kt
@@ -10,7 +10,7 @@ object Day9 : DayOf2024(9) {
   override fun first(): Any? {
     val sizes = data.map { it.digitToInt() }
     val length = sizes.sum()
-    val row = IntArray(length)
+    val row = IntArray(length) { Int.MAX_VALUE }
 
     val chunks = sizes
       .mapIndexed { index, size ->
@@ -44,8 +44,48 @@ object Day9 : DayOf2024(9) {
       }
     }
 
-    return row.takeWhile { it != Int.MAX_VALUE }
-      .mapIndexed { index, value -> index * value.toLong() }
+    return row
+      .mapIndexed { index, value -> if (value != Int.MAX_VALUE) index * value.toLong() else 0 }
+      .sum()
+  }
+
+  override fun second(): Any? {
+    val sizes = data.map { it.digitToInt() }
+    val length = sizes.sum()
+    val row = IntArray(length) { Int.MAX_VALUE }
+
+    val chunks = sizes
+      .mapIndexed { index, size ->
+        size to if (index % 2 == 0) index / 2 else Int.MAX_VALUE
+      }
+
+    val starts = chunks.scan(0) { acc, (size, _) -> acc + size }.dropLast(1)
+
+    val map = starts
+      .zip(chunks) { start, (size, value) ->
+        Triple(start, size, value)
+      }
+      .toTypedArray()
+
+    for (curr in map.lastIndex downTo 0 step 2) {
+      val (itemStart, itemSize, itemValue) = map[curr]
+      val slot = map.indexOfFirst { (start, size, value) ->
+        value == Int.MAX_VALUE && start < itemStart && size >= itemSize
+      }
+
+      if (slot != -1) {
+        val (start, size, _) = map[slot]
+        map[curr] = Triple(start, itemSize, itemValue)
+        map[slot] = map[slot].copy(first = start + itemSize, second = size - itemSize)
+      }
+    }
+
+    map.forEach { (start, size, value) ->
+      row.fill(value, start, start + size)
+    }
+
+    return row
+      .mapIndexed { index, value -> if (value != Int.MAX_VALUE) index * value.toLong() else 0 }
       .sum()
   }
 }
