e61f2a432cc80b6e870495ce329429340747472c
Oleg Godovykh <0legg@users.noreply.github.com>
2024-12-09 21:16:42 -0800

    solve 10th day 2nd task

--- year2024/src/main/kotlin/net/olegg/aoc/year2024/day10/Day10.kt
+++ year2024/src/main/kotlin/net/olegg/aoc/year2024/day10/Day10.kt
@@ -4,17 +4,18 @@ import net.olegg.aoc.someday.SomeDay
 import net.olegg.aoc.utils.Directions
 import net.olegg.aoc.utils.Vector2D
 import net.olegg.aoc.utils.get
+import net.olegg.aoc.utils.set
 import net.olegg.aoc.year2024.DayOf2024
 
 /**
  * See [Year 2024, Day 10](https://adventofcode.com/2024/day/10)
  */
 object Day10 : DayOf2024(10) {
-  override fun first(): Any? {
-    val heights = lines.map { row ->
-      row.map { it.digitToInt() }
-    }
+  private val heights = lines.map { row ->
+    row.map { it.digitToInt() }
+  }
 
+  override fun first(): Any? {
     val heads = heights.flatMapIndexed { y, row ->
       row.mapIndexedNotNull { x, c ->
         if (c == 0) Vector2D(x, y) else null
@@ -40,6 +41,48 @@ object Day10 : DayOf2024(10) {
       }
 
       ends.size
+    }
+  }
+
+  override fun second(): Any? {
+    val heads = heights.flatMapIndexed { y, row ->
+      row.mapIndexedNotNull { x, c ->
+        if (c == 0) Vector2D(x, y) else null
+      }
+    }
+
+    return heads.sumOf { head ->
+      val ends = mutableSetOf<Vector2D>()
+
+      val options = heights.map { it.map { 0 }.toMutableList() }
+      options[head] = 1
+
+      val queue = ArrayDeque(listOf(head))
+      val visited = mutableSetOf<Vector2D>()
+
+      while (queue.isNotEmpty()) {
+        val curr = queue.removeFirst()
+        if (curr in visited) {
+          continue
+        }
+        visited += curr
+        val height = heights[curr]!!
+
+        if (heights[curr] == 9) {
+          ends += curr
+        } else {
+          val next = Directions.NEXT_4
+            .map { curr + it.step }
+            .filter { heights[it] == height + 1 }
+
+          next.forEach {
+            options[it] = options[it]!! + options[curr]!!
+          }
+          queue += next
+        }
+      }
+
+      ends.sumOf { options[it]!! }
     }
   }
 }
