fde0d55e7e7d05ea95bb0368fd14431d7ef062d9
Oleg Godovykh <0legg@users.noreply.github.com>
2024-12-14 21:57:49 -0800

    solve 15th day 2nd task

--- year2024/src/main/kotlin/net/olegg/aoc/year2024/day15/Day15.kt
+++ year2024/src/main/kotlin/net/olegg/aoc/year2024/day15/Day15.kt
@@ -64,6 +64,58 @@ object Day15 : DayOf2024(15) {
 
     return (finalBoxes - finalRobot).sumOf { it.y * 100 + it.x }
   }
+
+  override fun second(): Any? {
+    val (rawMap, rawOps) = data.split("\n\n")
+    val ops = rawOps.filter { it in dirs }
+
+    val startMap = rawMap.lines().map { it.toList() }
+    val startRobot = startMap.find('@')!!.let { Vector2D(it.x * 2, it.y) }
+    val startBoxes = buildSet {
+      startMap.forEachIndexed { y, row ->
+        row.forEachIndexed { x, c ->
+          if (c == 'O') {
+            add(Vector2D(2 * x, y) to Vector2D(2 * x + 1, y))
+          }
+        }
+      }
+    }
+
+    val walls = buildSet {
+      startMap.forEachIndexed { y, row ->
+        row.forEachIndexed { x, c ->
+          if (c == '#') {
+            add(Vector2D(2 * x, y))
+            add(Vector2D(2 * x + 1, y))
+          }
+        }
+      }
+    }
+
+    val (_, finalBoxes) = ops.fold(startRobot to startBoxes) { (robot, boxes), op ->
+      val dir = dirs.getValue(op)
+
+      val movingBlocks = generateSequence(setOf(robot)) { curr ->
+        val next = curr.map { it + dir.step }
+        val front = boxes.filter { it.first in next || it.second in next }
+
+        curr + front.flatMap { it.toList() }
+      }
+        .zipWithNext()
+        .first { it.first == it.second }
+        .first
+
+      if (movingBlocks.any { it + dir.step in walls }) {
+        robot to boxes
+      } else {
+        val (boxesToMove, boxesToStay) = boxes.partition { it.first in movingBlocks }
+
+        (robot + dir.step) to (boxesToStay + boxesToMove.map { it.first + dir.step to it.second + dir.step }).toSet()
+      }
+    }
+
+    return finalBoxes.sumOf { it.first.y * 100 + it.first.x }
+  }
 }
 
 fun main() = SomeDay.mainify(Day15)
