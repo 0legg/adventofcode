6b34c4cf9343f355c7583783d82bd8e3b7c3fc01
Oleg Godovykh <0legg@users.noreply.github.com>
2024-12-15 21:32:32 -0800

    solve 16th day 2nd task

--- year2024/src/main/kotlin/net/olegg/aoc/year2024/day16/Day16.kt
+++ year2024/src/main/kotlin/net/olegg/aoc/year2024/day16/Day16.kt
@@ -44,6 +44,60 @@ object Day16 : DayOf2024(16) {
 
     return -1
   }
+
+  override fun second(): Any? {
+    val start = matrix.find('S')!!
+    val end = matrix.find('E')!!
+
+    val startConfig = Triple(start, Directions.R, 0)
+    val queue = PriorityQueue<Triple<Vector2D, Directions, Int>>(compareBy { it.third })
+    queue.add(startConfig)
+    val seen = mutableSetOf<Pair<Vector2D, Directions>>()
+    val bestSeen = mutableSetOf<Triple<Vector2D, Directions, Int>>()
+    var best = Int.MAX_VALUE
+
+    while (queue.isNotEmpty()) {
+      val point = queue.remove()
+      val (curr, dir, length) = point
+      if (length > best) {
+        break
+      }
+      if (curr == end) {
+        best = length
+      }
+      val config = curr to dir
+      if (config in seen) {
+        continue
+      }
+      seen += config
+      bestSeen += point
+
+      if (matrix[curr + dir.step] in empty) {
+        queue.add(Triple(curr + dir.step, dir, length + 1))
+      }
+      queue.add(Triple(curr, CW[dir]!!, length + 1000))
+      queue.add(Triple(curr, CCW[dir]!!, length + 1000))
+    }
+
+    println(seen.size)
+
+    val route = mutableSetOf<Vector2D>()
+    val traceback = PriorityQueue<Triple<Vector2D, Directions, Int>>(compareByDescending { it.third })
+    traceback.addAll(bestSeen.filter { it.first == end && it.third == best })
+    while (traceback.isNotEmpty()) {
+      val point = traceback.remove()
+      val (curr, dir, length) = point
+      route += curr
+
+      traceback += listOf(
+        Triple(curr - dir.step, dir, length - 1),
+        Triple(curr, CW[dir]!!, length - 1000),
+        Triple(curr, CCW[dir]!!, length - 1000),
+      ).filter { it in bestSeen }
+    }
+
+    return route.size
+  }
 }
 
 fun main() = SomeDay.mainify(Day16)
