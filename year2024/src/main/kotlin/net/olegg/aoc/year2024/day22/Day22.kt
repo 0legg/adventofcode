03e82f24fc8d38dcbd200cf976ddd1e2912f1209
Oleg Godovykh <0legg@users.noreply.github.com>
2024-12-21 21:33:35 -0800

    solve 22nd day 2nd task

--- year2024/src/main/kotlin/net/olegg/aoc/year2024/day22/Day22.kt
+++ year2024/src/main/kotlin/net/olegg/aoc/year2024/day22/Day22.kt
@@ -22,6 +22,34 @@ object Day22 : DayOf2024(22) {
           .first()
       }
   }
+
+  override fun second(): Any? {
+    val prices = lines
+      .map { it.toLong() }
+      .map { number ->
+        val nums = generateSequence(number) { curr ->
+          val first = (curr xor (curr shl 6)) and modulo
+          val second = (first xor (first shr 5)) and modulo
+          val third = (second xor (second shl 11)) and modulo
+          third
+        }
+          .map { curr -> curr % 10 }
+          .take(2001)
+          .toList()
+
+        val diffs = nums.zipWithNext { a, b -> b - a }
+
+        val pairs = diffs.windowed(4).asReversed().zip(nums.asReversed()).reversed()
+
+        pairs.groupBy { it.first }.mapValues { it.value.first().second }
+      }
+
+    val allDiffs = prices.flatMap { currPrices -> currPrices.keys }.toSet()
+
+    return allDiffs.maxOf { diff ->
+      prices.sumOf { currPrices -> (currPrices[diff] ?: 0) }
+    }
+  }
 }
 
 fun main() = SomeDay.mainify(Day22)
