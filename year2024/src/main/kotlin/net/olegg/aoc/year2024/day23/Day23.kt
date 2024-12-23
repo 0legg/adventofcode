1dc73d935bed28595d0664450b013467b3251867
Oleg Godovykh <0legg@users.noreply.github.com>
2024-12-22 21:35:35 -0800

    solve 23rd day 2nd task

--- year2024/src/main/kotlin/net/olegg/aoc/year2024/day23/Day23.kt
+++ year2024/src/main/kotlin/net/olegg/aoc/year2024/day23/Day23.kt
@@ -29,6 +29,44 @@ object Day23 : DayOf2024(23) {
       set.any { it.startsWith("t") }
     }
   }
+
+  override fun second(): Any? {
+    val connections = lines.map { it.split("-").sorted() }
+    val nodes = connections.flatMap { it.toList() }.toSet()
+    val neighbors = nodes.associateWith { node ->
+      connections.filter { node in it }.flatten().toSet() - node
+    }
+
+    val cliques = findCliques(emptySet(), nodes, emptySet(), neighbors)
+
+    return cliques.maxBy { it.size }.toSortedSet().joinToString(",")
+  }
+
+  private fun findCliques(
+    current: Set<String>,
+    potential: Set<String>,
+    excluded: Set<String>,
+    graph: Map<String, Set<String>>,
+  ): Set<Set<String>> {
+    return if (potential.isEmpty() && excluded.isEmpty()) {
+      setOf(current)
+    } else {
+      val currPotential = potential.toMutableSet()
+      val currExcluded = excluded.toMutableSet()
+      val cliques = mutableSetOf<Set<String>>()
+      while (currPotential.isNotEmpty()) {
+        val next = currPotential.random()
+        val nextNext = graph[next].orEmpty()
+        val newCurrent = current + next
+        val newPotential = currPotential.filter { it in nextNext }.toSet()
+        val newExcluded = currExcluded.filter { it in nextNext }.toSet()
+        cliques += findCliques(newCurrent, newPotential, newExcluded, graph)
+        currPotential -= next
+        currExcluded += next
+      }
+      cliques
+    }
+  }
 }
 
 fun main() = SomeDay.mainify(Day23)
