5ec6dbf9858b2f9b5a4026b3b7d450f71931c010
Oleg Godovykh <0legg@users.noreply.github.com>
2025-11-30 21:24:22 -0800

    solve 1st day 2nd task

--- year2025/src/main/kotlin/net/olegg/aoc/year2025/day1/Day1.kt
+++ year2025/src/main/kotlin/net/olegg/aoc/year2025/day1/Day1.kt
@@ -10,12 +10,32 @@ object Day1 : DayOf2025(1) {
   override fun first(): Any? {
     return lines.scan(50) { pos, op ->
       val dist = op.drop(1).toInt()
-      when (op[0]) {
-        'L' -> ((pos - dist) % 100 + 100) % 100
-        'R' -> ((pos + dist) % 100 + 100) % 100
+      val rawPos = when (op[0]) {
+        'L' -> pos - dist
+        'R' -> pos + dist
         else -> pos
       }
+      (rawPos % 100 + 100) % 100
     }.count { it == 0 }
+  }
+
+  override fun second(): Any? {
+    return lines.scan(50 to 0) { (pos, _), op ->
+      val dist = op.drop(1).toInt()
+      val rawPos = when (op[0]) {
+        'L' -> pos - dist
+        'R' -> pos + dist
+        else -> pos
+      }
+      val actualPos = (rawPos % 100 + 100) % 100
+
+      val switch = when (op[0]) {
+        'L' -> (-rawPos + 100) / 100 - if (pos == 0) 1 else 0
+        'R' -> rawPos / 100
+        else -> 0
+      }
+      actualPos to switch
+    }.sumOf { it.second }
   }
 }
 
