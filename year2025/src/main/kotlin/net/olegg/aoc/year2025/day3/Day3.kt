c1f3c999c4c3e10d70102a0579e10736d720ce00
Oleg Godovykh <0legg@users.noreply.github.com>
2025-12-02 21:13:41 -0800

    solve 3rd day 2nd task

--- year2025/src/main/kotlin/net/olegg/aoc/year2025/day3/Day3.kt
+++ year2025/src/main/kotlin/net/olegg/aoc/year2025/day3/Day3.kt
@@ -1,7 +1,6 @@
 package net.olegg.aoc.year2025.day3
 
 import net.olegg.aoc.someday.SomeDay
-import net.olegg.aoc.utils.pairs
 import net.olegg.aoc.year2025.DayOf2025
 
 /**
@@ -9,9 +8,25 @@ import net.olegg.aoc.year2025.DayOf2025
  */
 object Day3 : DayOf2025(3) {
   override fun first(): Any? {
-    return lines.sumOf { line ->
-      line.toList().pairs().maxOf { "${it.first}${it.second}".toInt() }
+    return lines.sumOf { joltage(it, 2) }
+  }
+
+  override fun second(): Any? {
+    return lines.sumOf { joltage(it, 12) }
+  }
+
+  private fun joltage(line: String, size: Int): Long {
+    val range = 0..size
+    val best = line.fold(mapOf(0 to 0L)) { acc, char ->
+      val digit = char.digitToInt()
+      val next = acc.map { it.key + 1 to it.value * 10 + digit }.toMap()
+
+      range.associateWith {
+        maxOf(acc[it] ?: 0, next[it] ?: 0)
+      }
     }
+
+    return best.getOrDefault(size, 0)
   }
 }
 
